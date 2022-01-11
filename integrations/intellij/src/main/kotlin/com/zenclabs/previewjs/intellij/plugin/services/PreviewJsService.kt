d4f3aa61bce6c8c670a094e2bb0659d40c17e9be
François Wouts <f@zenc.io>
2022-01-11 18:21:06 +1100

    release: @previewjs/app v1.0.1

--- integrations/intellij/src/main/kotlin/com/zenclabs/previewjs/intellij/plugin/services/PreviewJsService.kt
+++ integrations/intellij/src/main/kotlin/com/zenclabs/previewjs/intellij/plugin/services/PreviewJsService.kt
@@ -101,7 +101,7 @@ ${e.stackTraceToString()}""",
         builder.environment()["PORT"] = "$PORT"
         builder.environment()["PREVIEWJS_INTELLIJ_VERSION"] = plugin.getVersion()
         builder.environment()["PREVIEWJS_PACKAGE_NAME"] = "@previewjs/app"
-        builder.environment()["PREVIEWJS_PACKAGE_VERSION"] = "1.0.0"
+        builder.environment()["PREVIEWJS_PACKAGE_VERSION"] = "1.0.1"
         println("Preview.js control plane errors will be logged to $errorLogFilePath")
         val process = builder.start()
         controlPlaneProcess = process
