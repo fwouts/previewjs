d7bc716e3d9c385dae6b17bbcdee5d4c26305bae
François Wouts <f@zenc.io>
2022-02-12 21:08:52 +1100

    chore: remove unused code in test apps (#183)

--- smoke-test-apps/cra-styled-components/src/index.tsx
+++ smoke-test-apps/cra-styled-components/src/index.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { createGlobalStyle } from "styled-components";
+import App from "./App";
 
 const GlobalStyle = createGlobalStyle`
 body {
@@ -27,8 +26,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
