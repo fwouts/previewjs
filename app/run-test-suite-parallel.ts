5432ae2c8fa3ffdbcb397625d7939b199df2ce7a
François Wouts <f@zenc.io>
2022-02-13 13:45:52 +1100

    switch to process-level parallelisation

---
+++ app/run-test-suite-parallel.ts
@@ -0,0 +1,41 @@
+import execa from "execa";
+import playwright from "playwright";
+
+async function main() {
+  const headless = process.env["HEADLESS"] !== "0";
+  const browser = await playwright.chromium.launchServer({
+    headless,
+    devtools: !headless,
+  });
+  const groupCount = parseInt(process.env["GROUP_COUNT"] || "1");
+  const processes: execa.ExecaChildProcess[] = [];
+  for (let i = 0; i < groupCount; i++) {
+    processes.push(
+      execa.command("pnpm test:group", {
+        env: {
+          ...process.env,
+          CHROMIUM_WS_ENDPOINT: browser.wsEndpoint(),
+          GROUP_INDEX: i.toString(10),
+          GROUP_COUNT: groupCount.toString(10),
+        },
+        stdout: "inherit",
+        stderr: "inherit",
+      })
+    );
+  }
+  let failed = false;
+  try {
+    const results = await Promise.all(processes);
+    const anyFailed = results.reduce((acc, curr) => {
+      return acc || curr.failed;
+    }, false);
+    failed = anyFailed;
+  } finally {
+    await browser.close();
+  }
+  if (failed) {
+    process.exit(1);
+  }
+}
+
+main().catch();
--- app/run-test-suite.ts
+++ app/run-test-suite.ts
@@ -5,65 +5,39 @@ import { runTests } from "./testing";
 import testSuitesPromises from "./tests";
 
 async function main() {
-  const headless = process.env["HEADLESS"] !== "0";
-  const groupCount = parseInt(process.env["GROUP_COUNT"] || "10");
-  const browser = await playwright.chromium.launch({
-    headless,
-    devtools: !headless,
-  });
-  try {
-    const promises: Array<
-      Promise<{
-        testCasesCount: number;
-        failedTests: string[];
-      }>
-    > = [];
-    const startTimeMillis = Date.now();
-    const testSuites = await Promise.all(testSuitesPromises);
-    for (let groupIndex = 0; groupIndex < groupCount; groupIndex++) {
-      promises.push(
-        runTests({
-          browser,
-          setupEnvironment,
-          testSuites: testSuites.filter(
-            (_, index) => index % groupCount === groupIndex
-          ),
-          filters: process.argv.slice(2),
-          outputDirPath: path.join(__dirname, "tests"),
-          port: 8100 + groupIndex,
-        })
-      );
-    }
-    const { testCasesCount, failedTests } = (
-      await Promise.all(promises)
-    ).reduce(
-      (acc, curr) => {
-        acc.testCasesCount += curr.testCasesCount;
-        acc.failedTests.push(...curr.failedTests);
-        return acc;
-      },
-      {
-        testCasesCount: 0,
-        failedTests: [],
-      }
-    );
-    const totalDurationMillis = Date.now() - startTimeMillis;
-    console.log(
-      `Test summary:\n${testCasesCount} test cases run, ${
-        failedTests.length
-      } failed.\nTotal time: ${totalDurationMillis / 1000}s.`
-    );
-    if (failedTests.length > 0) {
-      console.error(`The following tests failed:`);
-      for (const name of failedTests) {
-        console.error(`• ${name}`);
-      }
-      process.exit(1);
-    }
-    process.exit(0);
-  } finally {
-    await browser.close();
+  const chromiumWsEndpoint = process.env["CHROMIUM_WS_ENDPOINT"];
+  if (!chromiumWsEndpoint) {
+    throw new Error(`Missing env variable: CHROMIUM_WS_ENDPOINT`);
   }
+  const groupCount = parseInt(process.env["GROUP_COUNT"] || "1");
+  const groupIndex = parseInt(process.env["GROUP_INDEX"] || "0");
+  const browser = await playwright.chromium.connect(chromiumWsEndpoint);
+  const startTimeMillis = Date.now();
+  const testSuites = await Promise.all(testSuitesPromises);
+  const { testCasesCount, failedTests } = await runTests({
+    browser,
+    setupEnvironment,
+    testSuites: testSuites.filter(
+      (_, index) => index % groupCount === groupIndex
+    ),
+    filters: process.argv.slice(2),
+    outputDirPath: path.join(__dirname, "tests"),
+    port: 8100 + groupIndex,
+  });
+  const totalDurationMillis = Date.now() - startTimeMillis;
+  console.log(
+    `Test summary (group ${groupIndex}/${groupCount}):\n${testCasesCount} test cases run, ${
+      failedTests.length
+    } failed.\nTotal time: ${totalDurationMillis / 1000}s.`
+  );
+  if (failedTests.length > 0) {
+    console.error(`The following tests failed:`);
+    for (const name of failedTests) {
+      console.error(`• ${name}`);
+    }
+    process.exit(1);
+  }
+  process.exit(0);
 }
 
 main().catch((e) => {
