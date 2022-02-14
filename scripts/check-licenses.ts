ac1ee4907cc2c5104c5b11f9e0dc01d3eaebb402
François Wouts <f@zenc.io>
2022-02-14 17:04:40 +1100

    test: set up additional false negative testing for licenses check (#201)

--- scripts/check-licenses.ts
+++ scripts/check-licenses.ts
@@ -1,34 +1,84 @@
 import checker from "license-checker";
 import path from "path";
+import { inspect } from "util";
 
-checker.init(
-  {
-    start: path.join(__dirname, ".."),
-    // @ts-ignore
-    exclude: [
-      "Apache-2.0",
-      "BSD-2-Clause",
-      "BSD-3-Clause",
-      "CC-BY-4.0",
-      "ISC",
-      "MIT",
-      "MPL-2.0",
-    ].join(","),
-  },
-  (err, modules) => {
-    if (err) {
-      console.error(err);
-      process.exit(1);
-    }
-    modules = Object.fromEntries(
-      Object.entries(modules).filter(
-        ([name]) => !name.startsWith("@previewjs/")
-      )
-    );
-    if (Object.entries(modules).length > 0) {
-      console.error(`Some packages have incompatible licenses:`);
-      console.error(modules);
-      process.exit(1);
+const start = path.join(__dirname, "..");
+
+async function main() {
+  const allPackages = await extractPackages(start, []);
+  // Check that we don't get false negatives by missing packages.
+  for (const packageName of [
+    "@previewjs/app",
+    "mobx-react-lite",
+    "@fortawesome/react-fontawesome",
+    "@svgr/core",
+  ]) {
+    if (!allPackages.find(({ name }) => name === packageName)) {
+      throw new Error(`Expected to find package: ${packageName}`);
     }
   }
-);
+  const incompatibleLicensePackages = await extractPackages(start, [
+    "Apache-2.0",
+    "BSD-2-Clause",
+    "BSD-3-Clause",
+    "CC-BY-4.0",
+    "ISC",
+    "MIT",
+    "MPL-2.0",
+  ]);
+  const nonPreviewJsPackages = incompatibleLicensePackages.filter(
+    ({ name }) => !name.startsWith("@previewjs/")
+  );
+  if (nonPreviewJsPackages.length > 0) {
+    throw new Error(
+      `Some packages have incompatible licenses:\n${inspect(
+        nonPreviewJsPackages
+      )}`
+    );
+  }
+  console.log(`All is good!`);
+}
+
+main().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
+
+type PackageInfo = {
+  name: string;
+  version: string;
+  licenses?: string | string[];
+};
+
+async function extractPackages(
+  start: string,
+  excludeLicenses: string[]
+): Promise<PackageInfo[]> {
+  return new Promise<PackageInfo[]>((resolve, reject) =>
+    checker.init(
+      {
+        start,
+        // @ts-ignore
+        exclude: excludeLicenses.join(","),
+      },
+      (err, modules) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        const packages: PackageInfo[] = [];
+        for (const [packageNameWithVersion, info] of Object.entries(modules)) {
+          const atSignPosition = packageNameWithVersion.lastIndexOf("@");
+          const name = packageNameWithVersion.substring(0, atSignPosition);
+          const version = packageNameWithVersion.substring(atSignPosition + 1);
+          packages.push({
+            name,
+            version,
+            licenses: info.licenses,
+          });
+        }
+        resolve(packages);
+      }
+    )
+  );
+}
