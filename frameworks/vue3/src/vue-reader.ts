f58fa97a305347aafff1b72d54653d2987bd73ef
François Wouts <f@zenc.io>
2022-02-22 15:58:50 +1100

    fix: ensure Vue 3 props are detected with defineComponent() and setup method

--- frameworks/vue3/src/vue-reader.ts
+++ frameworks/vue3/src/vue-reader.ts
@@ -8,6 +8,7 @@ import {
 } from "@previewjs/core/vfs";
 import { parse } from "@vue/compiler-sfc";
 import path from "path";
+import ts from "typescript";
 
 /**
  * Returns a reader that returns a virtual TypeScript file
@@ -75,23 +76,90 @@ class VueTypeScriptReader implements Reader {
 
 function convertToTypeScript(vueTemplateSource: string, name: string) {
   const parsed = parse(vueTemplateSource);
-  return parsed.descriptor.scriptSetup
-    ? `import { defineProps } from '@vue/runtime-core';\n${parsed.descriptor.scriptSetup.content}`
-    : parsed.descriptor.script
-    ? `${parsed.descriptor.script.content.replace(
-        /export\s+default/,
-        "const pjs_component ="
-      )}
+  if (parsed.descriptor.scriptSetup) {
+    return `import { defineProps } from '@vue/runtime-core';\n${parsed.descriptor.scriptSetup.content}`;
+  } else if (parsed.descriptor.script) {
+    // TODO: Ignore non-JS compatible languages (same with Vue 2).
+    const scriptContent = parsed.descriptor.script.content;
+    const sourceFile = ts.createSourceFile(
+      `${name}.vue.ts`,
+      scriptContent,
+      ts.ScriptTarget.Latest
+    );
+    const transformation = ts.transform(sourceFile, [
+      (context) => (sourceFile) => {
+        if (ts.isSourceFile(sourceFile)) {
+          return ts.visitEachChild(
+            sourceFile,
+            (node) => {
+              if (ts.isExportAssignment(node)) {
+                return ts.factory.createVariableStatement(
+                  [],
+                  ts.factory.createVariableDeclarationList(
+                    [
+                      ts.factory.createVariableDeclaration(
+                        "pjs_component",
+                        undefined,
+                        undefined,
+                        ts.factory.createAsExpression(
+                          extractDefineComponentArgument(node.expression),
+                          ts.factory.createTypeReferenceNode("const")
+                        )
+                      ),
+                    ],
+                    ts.NodeFlags.Const
+                  )
+                );
+              }
+              return node;
+            },
+            context
+          );
+        }
+        return sourceFile;
+      },
+    ]);
+    const transformed = ts
+      .createPrinter()
+      .printFile(transformation.transformed[0]!);
+    transformation.dispose();
+    return `
+${transformed}
 
-import type { Component as PJS_Component } from "@vue/runtime-core";
+import type { PropType as PJS_PropType } from "@vue/runtime-core";
 
-type PJS_ExtractProps<T> = T extends PJS_Component<infer S>
-  ? S extends { $props: unknown }
-    ? S["$props"]
-    : never
-  : never;
-
+type PJS_TypeOrUnion<T> = PJS_PropType<T> | ReadonlyArray<PJS_PropType<T>>;
+type PJS_OptionalPropType<T> = PJS_TypeOrUnion<T> | {type: PJS_TypeOrUnion<T>; required?: false};
+type PJS_RequiredPropType<T> = {type: PJS_TypeOrUnion<T>; required: true};
+type PJS_OptionalPropsKeys<T> = {
+  [K in keyof T]: T[K] extends PJS_OptionalPropType<any> ? K : never;
+}[keyof T];
+type PJS_RequiredPropsKeys<T> = {
+  [K in keyof T]: T[K] extends PJS_RequiredPropType<any> ? K : never;
+}[keyof T];
+type PJS_CombinedProps<T> = T extends readonly [...any] ? {
+  [K in T[number]]: unknown
+} : ({
+  [K in PJS_OptionalPropsKeys<T>]?: T[K] extends PJS_OptionalPropType<infer S> ? S : never;
+} & {
+  [K in PJS_RequiredPropsKeys<T>]: T[K] extends PJS_RequiredPropType<infer S> ? S : never;
+});
+type PJS_ExtractProps<T> = T extends { props: any } ? PJS_CombinedProps<T['props']> : {}
 type PJS_Props = PJS_ExtractProps<typeof pjs_component>;
-`
-    : "";
+`;
+  } else {
+    return "";
+  }
+}
+
+function extractDefineComponentArgument(node: ts.Expression): ts.Expression {
+  if (
+    ts.isCallExpression(node) &&
+    ts.isIdentifier(node.expression) &&
+    node.expression.text === "defineComponent" &&
+    node.arguments.length > 0
+  ) {
+    return node.arguments[0]!;
+  }
+  return node;
 }
