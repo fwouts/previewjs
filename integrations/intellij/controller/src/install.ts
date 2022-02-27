678a6a37fa8532b5272468ffcebb97ab4e57d46e
François Wouts <f@zenc.io>
2022-02-27 18:09:41 +1100

    fix(intellij): prevent crash on install

--- integrations/intellij/controller/src/install.ts
+++ integrations/intellij/controller/src/install.ts
@@ -7,15 +7,6 @@ main().catch((e) => {
 });
 
 async function main() {
-  const packageName = process.env.PREVIEWJS_PACKAGE_NAME;
-  if (!packageName) {
-    throw new Error(`Missing environment variable: PREVIEWJS_PACKAGE_NAME`);
-  }
-  const packageVersion = process.env.PREVIEWJS_PACKAGE_VERSION;
-  if (!packageVersion) {
-    throw new Error(`Missing environment variable: PREVIEWJS_PACKAGE_VERSION`);
-  }
-
   await install({
     installDir: path.join(__dirname, "installed"),
     onOutput: (chunk) => process.stdout.write(chunk),
