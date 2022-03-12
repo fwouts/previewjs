672d86a78aeea50a62ae19021f0159774e159cf7
François Wouts <f@zenc.io>
2022-03-12 17:32:50 +1100

    Delete test-suites.ts

--- pro/test-suites.ts
+++
@@ -1,7 +0,0 @@
-import ossTestSuites from "@previewjs/app/tests";
-import { multipleComponentsTests } from "./src/tests/multiple-components.spec";
-import { storiesTests } from "./src/tests/stories.spec";
-
-const testSuites = [multipleComponentsTests, storiesTests, ...ossTestSuites];
-
-export default testSuites;
