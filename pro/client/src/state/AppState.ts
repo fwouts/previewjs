1d7b072f532172d9ba99ed1d7ead5a5752b4dc01
François Wouts <f@zenc.io>
2022-03-12 15:28:48 +1100

    remove licensing

--- pro/client/src/state/AppState.ts
+++ pro/client/src/state/AppState.ts
@@ -3,13 +3,9 @@ import { WebApi } from "@previewjs/app/client/src/api/web";
 import { PersistedStateController } from "@previewjs/app/client/src/PersistedStateController";
 import { PreviewState } from "@previewjs/app/client/src/PreviewState";
 import { makeAutoObservable } from "mobx";
-import { LicenseModalState } from "../license-modal/LicenseModalState";
-import { LicenseState } from "./LicenseState";
 
 export class AppState {
   readonly preview: PreviewState;
-  readonly license: LicenseState;
-  readonly licenseModal: LicenseModalState;
 
   private readonly persistedStateController: PersistedStateController;
 
@@ -21,13 +17,10 @@ export class AppState {
       webApi,
       this.persistedStateController
     );
-    this.license = new LicenseState(webApi, this.persistedStateController);
-    this.licenseModal = new LicenseModalState(webApi, this.license);
   }
 
   async start() {
     await this.preview.start();
-    await this.license.start();
   }
 
   async stop() {
