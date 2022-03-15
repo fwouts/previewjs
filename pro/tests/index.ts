1aba487d41f88b608d5d3d8bd04640df0898a91e
François Wouts <f@zenc.io>
2022-03-16 09:28:53 +1100

    remove stories tests for now

--- pro/tests/index.ts
+++ pro/tests/index.ts
@@ -1,7 +1,6 @@
 import ossTestSuites from "@previewjs/app/tests";
 import { multipleComponentsTests } from "./multiple-components.spec";
-import { storiesTests } from "./stories.spec";
 
-const testSuites = [multipleComponentsTests, storiesTests, ...ossTestSuites];
+const testSuites = [multipleComponentsTests, ...ossTestSuites];
 
 export default testSuites;
