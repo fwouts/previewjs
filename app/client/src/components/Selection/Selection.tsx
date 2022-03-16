93eae1c42097aa43d365c593bd009e925e41db66
François Wouts <f@zenc.io>
2022-03-16 13:22:12 +1100

    refactor: forward ref of Selection component

--- app/client/src/components/Selection/Selection.tsx
+++ app/client/src/components/Selection/Selection.tsx
@@ -1,34 +1,41 @@
 import { faCode, IconDefinition } from "@fortawesome/free-solid-svg-icons";
 import { observer } from "mobx-react-lite";
-import React from "react";
+import React, {ForwardedRef, forwardRef} from "react";
 import { Pill } from "../../design/Pill";
 import { PreviewState } from "../../PreviewState";
 
 export const Selection = observer(
-  ({
-    state,
-    icon = faCode,
-  }: {
-    state: PreviewState;
-    icon?: IconDefinition;
-  }) => {
-    if (!state.component) {
-      return null;
+  forwardRef(
+    (
+      {
+        state,
+        icon = faCode,
+      }: {
+        state: PreviewState;
+        icon?: IconDefinition;
+      },
+      ref: ForwardedRef<HTMLDivElement>
+    ) => {
+      if (!state.component) {
+        return null;
+      }
+      return (
+        <Pill
+          ref={ref}
+          key="component"
+          icon={icon}
+          label={state.component.name}
+          loading={!state.component.details?.variants}
+          buttons={
+            state.component.details?.variants?.filter(
+              (v) => !v.isEditorDriven
+            ) || undefined
+          }
+          selectedButtonKey={state.component.variantKey}
+          onClick={() => state.setVariant("custom")}
+          onButtonClicked={state.setVariant.bind(state)}
+        />
+      );
     }
-    return (
-      <Pill
-        key="component"
-        icon={icon}
-        label={state.component.name}
-        loading={!state.component.details?.variants}
-        buttons={
-          state.component.details?.variants?.filter((v) => !v.isEditorDriven) ||
-          undefined
-        }
-        selectedButtonKey={state.component.variantKey}
-        onClick={() => state.setVariant("custom")}
-        onButtonClicked={state.setVariant.bind(state)}
-      />
-    );
-  }
+  )
 );
