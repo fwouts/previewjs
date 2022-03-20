fda60fab6840fc9757f97cf412b4c2383c6554ad
François Wouts <f@zenc.io>
2022-03-21 10:38:59 +1100

    fix(react): prevent crash when using custom previews in an environment where `process` is not defined
    Signed-off-by: François Wouts <f@zenc.io>

--- frameworks/react/setup.mjs
+++ frameworks/react/setup.mjs
@@ -1,17 +1,18 @@
-export const setupPreviews = process.env.RUNNING_INSIDE_PREVIEWJS
-  ? (Component, previewProps) => {
-      if (typeof previewProps === "function") {
-        previewProps = previewProps();
+export const setupPreviews =
+  window.process && process.env && process.env.RUNNING_INSIDE_PREVIEWJS
+    ? (Component, previewProps) => {
+        if (typeof previewProps === "function") {
+          previewProps = previewProps();
+        }
+        Component.__previewjs_variants = [];
+        for (const [key, props] of Object.entries(previewProps)) {
+          Component.__previewjs_variants.push({
+            key,
+            label: key,
+            props,
+          });
+        }
       }
-      Component.__previewjs_variants = [];
-      for (const [key, props] of Object.entries(previewProps)) {
-        Component.__previewjs_variants.push({
-          key,
-          label: key,
-          props,
-        });
-      }
-    }
-  : () => {
-      // You read this right, it's a no-op.
-    };
+    : () => {
+        // You read this right, it's a no-op.
+      };
