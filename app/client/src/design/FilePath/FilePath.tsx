2b0903b642d294f41d32a06b9d02983ad79cf9a5
François Wouts <f@zenc.io>
2022-03-24 11:59:51 +1100

    design: ellipsize file path when it doesn't fit in top row

--- app/client/src/design/FilePath/FilePath.tsx
+++ app/client/src/design/FilePath/FilePath.tsx
@@ -1,15 +1,12 @@
 import React from "react";
 
 export const FilePath = ({ filePath }: { filePath: string }) => {
-  const segments = filePath.split(/[/\\]/);
   return (
-    <div className="flex-grow flex flex-row flex-wrap text-blue-100">
-      {segments.map((segment, i) => (
-        <React.Fragment key={i}>
-          {i > 0 && <span className="mx-1">/</span>}
-          <span className="whitespace-nowrap">{segment}</span>
-        </React.Fragment>
-      ))}
+    <div
+      className="flex-grow text-left text-blue-100 truncate"
+      style={{ direction: "rtl" }}
+    >
+      {filePath}
     </div>
   );
 };
