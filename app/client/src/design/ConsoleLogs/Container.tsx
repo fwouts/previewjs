40bac92a9c7d79d6df4f7fe505dad9e52762df13
François Wouts <f@zenc.io>
2022-03-24 12:09:00 +1100

    fix: ensure console logs scroll to bottom on each rerender (#338)

--- app/client/src/design/ConsoleLogs/Container.tsx
+++ app/client/src/design/ConsoleLogs/Container.tsx
@@ -6,19 +6,27 @@ export const Container = (props: {
   children: React.ReactNode[];
   onClear(): void;
 }) => {
-  const scrollToBottomRef = useRef<HTMLButtonElement | null>(null);
+  const scrollableContainerRef = useRef<HTMLDivElement | null>(null);
   useEffect(() => {
-    scrollToBottomRef.current?.scrollIntoView({
+    if (!scrollableContainerRef.current) {
+      return;
+    }
+    scrollableContainerRef.current.scroll({
       behavior: "smooth",
+      top: scrollableContainerRef.current.scrollHeight,
     });
   });
   return (
     <div className="flex flex-col min-h-0">
-      <div className="flex flex-col overflow-y-auto">{props.children}</div>
+      <div
+        ref={scrollableContainerRef}
+        className="flex flex-col overflow-y-auto"
+      >
+        {props.children}
+      </div>
       {props.children.length > 0 && (
         <button
           className="p-2 text-gray-400 border-t-2 border-gray-100 hover:text-gray-600"
-          ref={scrollToBottomRef}
           onClick={props.onClear}
         >
           <FontAwesomeIcon icon={faTimesCircle} /> Clear all
