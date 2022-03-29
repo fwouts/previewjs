a49112bdd33874797a2c1a76936daf35de24af11
François Wouts <f@zenc.io>
2022-03-29 15:43:36 +1100

    remove unnecessary margin

--- pro/client/src/design/VariantButton/VariantButton.tsx
+++ pro/client/src/design/VariantButton/VariantButton.tsx
@@ -8,7 +8,7 @@ export const VariantButton: React.FC<{
 }> = (props) => (
   <button
     className={clsx([
-      "self-stretch flex flex-row items-center text-blue-900 hover:bg-blue-200 my-2 ml-2 px-2 py-0.5 mr-2 text-sm font-semibold rounded whitespace-nowrap",
+      "self-stretch flex flex-row items-center text-blue-900 hover:bg-blue-200 my-2 px-2 py-0.5 mr-2 text-sm font-semibold rounded whitespace-nowrap",
       props.warning &&
         "bg-orange-300 hover:bg-orange-200 text-orange-900 hover:text-orange-800",
     ])}
