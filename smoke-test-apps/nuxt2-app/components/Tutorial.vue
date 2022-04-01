968d5cba5becb25e743e1f00bef2b2eb7cc15453
François Wouts <f@zenc.io>
2022-04-01 18:42:42 +1100

    test: further simplify Nuxt 2 test app to eliminate flakiness (#404)

--- smoke-test-apps/nuxt2-app/components/Tutorial.vue
+++ smoke-test-apps/nuxt2-app/components/Tutorial.vue
@@ -1,30 +1,5 @@
-<!-- Please remove this file from your project -->
 <template>
-  <div
-    class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0"
-  >
-    <link
-      href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css"
-      rel="stylesheet"
-    />
-    <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
-      <div class="mt-8 bg-white overflow-hidden p-6">
-        <h2 class="text-2xl leading-7 font-semibold">
-          Welcome to your Nuxt Application
-        </h2>
-        <p class="mt-3 text-gray-600">
-          We recommend you take a look at the
-          <a
-            href="https://nuxtjs.org"
-            target="_blank"
-            class="button--doc text-green-500 hover:underline"
-            >Nuxt documentation</a
-          >, whether you are new or have previous experience with the
-          framework.<br />
-        </p>
-      </div>
-    </div>
-  </div>
+  <div>Hello from Nuxt</div>
 </template>
 
 <script>
