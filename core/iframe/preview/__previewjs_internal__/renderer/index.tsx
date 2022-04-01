540bc9f21cb9776961acb22507d8859b12139047
François Wouts <f@zenc.io>
2022-04-01 12:39:27 +1100

    feat: support React 18 stable release

--- core/iframe/preview/__previewjs_internal__/renderer/index.tsx
+++ core/iframe/preview/__previewjs_internal__/renderer/index.tsx
@@ -1,4 +1,4 @@
-export function render(component: any, props: any) {
+export async function render(component: any, props: any) {
   // This is a dummy function that is replaced by
   // a framework-specific implementation at runtime.
   console.error("Dummy renderer was invoked!");
--- frameworks/react/preview/index.tsx
+++ frameworks/react/preview/index.tsx
@@ -1,32 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { ErrorBoundary } from "./ErrorBoundary";
+// @ts-ignore Vite is fine with this
+import { version } from "react/package.json";
 
-let root: any;
-export function render<P>(Renderer: React.ComponentType<P>, props: P) {
-  const container = document.getElementById("root");
-  if (!Renderer) {
-    if (root) {
-      root.unmount();
-      root = null;
-    } else {
-      ReactDOM.unmountComponentAtNode(container);
-    }
-    return;
-  }
-  if ("createRoot" in ReactDOM && !root) {
-    // React 18 API.
-    // See https://github.com/reactwg/react-18/discussions/6.
-    root = (ReactDOM as any).createRoot(container);
-  }
-  const element = ( // Ensure we get a fresh ErrorBoundary.
-    <ErrorBoundary key={Date.now()}>
-      <Renderer {...props} />
-    </ErrorBoundary>
-  );
-  if (root) {
-    root.render(element);
-  } else {
-    ReactDOM.render(element, container);
-  }
+const moduleName = parseInt(version) >= 18 ? "./render-18" : "./render-16";
+
+export async function render<P>(Renderer: React.ComponentType<P>, props: P) {
+  const { render } = await import(/* @vite-ignore */ moduleName);
+  return render(Renderer, props);
 }
