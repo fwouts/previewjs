55f6bb7fb8fb545c2ebc628b663f21c2aed06bde
François Wouts <f@zenc.io>
2022-04-05 22:49:15 +1000

    Remove framework directory

--- frameworks/solid/src/special-types.ts
+++
@@ -1,8 +0,0 @@
-import { functionType, NODE_TYPE, ValueType } from "@previewjs/type-analyzer";
-
-export const SOLID_SPECIAL_TYPES: Record<string, ValueType> = {
-  Component: functionType(NODE_TYPE),
-  Element: NODE_TYPE,
-  FunctionElement: NODE_TYPE,
-  Node: NODE_TYPE,
-};
