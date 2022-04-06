b2691b6496f264e88a66168dc6f04c20f628fb5e
François Wouts <f@zenc.io>
2022-04-06 15:10:10 +1000

    test: add smoke test apps for SolidJS

---
+++ smoke-test-apps/solid-js/src/App.jsx
@@ -0,0 +1,25 @@
+import styles from "./App.module.css";
+import logo from "./logo.svg";
+
+function App() {
+  return (
+    <div id="ready" class={styles.App}>
+      <header class={styles.header}>
+        <img src={logo} class={styles.logo} alt="logo" />
+        <p>
+          Edit <code>src/App.jsx</code> and save to reload.
+        </p>
+        <a
+          class={styles.link}
+          href="https://github.com/solidjs/solid"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Solid
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
