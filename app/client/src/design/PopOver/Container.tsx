4acf23012ab6ac686257382b89d1b3e631804a3d
François Wouts <f@zenc.io>
2022-04-12 23:12:01 +1000

    fix: use React.FC<{children: React.ReactNode}> explicitly

--- app/client/src/design/PopOver/Container.tsx
+++ app/client/src/design/PopOver/Container.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 
-export const Container: React.FC = ({ children }) => {
+export const Container: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
   return (
     <div className="absolute overflow-hidden bottom-0 right-0 z-50">
       {children}
