b5fc8bca114eede92a637df6bb23843f3481056b
François Wouts <f@zenc.io>
2022-05-15 12:33:53 +1000

    refactor: introduce SerializableValue (prerequisite for structured props editor) (#595)

---
+++ app/client/src/generators/generate-serializable-value.spec.ts
@@ -0,0 +1,421 @@
+import {
+  ANY_TYPE,
+  arrayType,
+  BOOLEAN_TYPE,
+  enumType,
+  functionType,
+  intersectionType,
+  literalType,
+  namedType,
+  NEVER_TYPE,
+  NODE_TYPE,
+  NULL_TYPE,
+  NUMBER_TYPE,
+  objectType,
+  optionalType,
+  promiseType,
+  recordType,
+  setType,
+  STRING_TYPE,
+  unionType,
+  UNKNOWN_TYPE,
+  VOID_TYPE,
+} from "@previewjs/type-analyzer";
+import { describe, expect, test } from "vitest";
+import { generateSerializableValue } from "./generate-serializable-value";
+
+describe("generateSerializableValue", () => {
+  test("simple props with object type", () => {
+    expect(
+      generateSerializableValue(
+        objectType({
+          foo: STRING_TYPE,
+        }),
+
+        {}
+      )
+    ).toMatchInlineSnapshot(`
+      {
+        "entries": {
+          "foo": {
+            "kind": "string",
+            "value": "foo",
+          },
+        },
+        "kind": "object",
+      }
+    `);
+  });
+
+  test("simple props with named type", () => {
+    expect(
+      generateSerializableValue(namedType("/foo.tsx:MyComponentProps"), {
+        "/foo.tsx:MyComponentProps": {
+          type: objectType({
+            foo: STRING_TYPE,
+          }),
+
+          parameters: {},
+        },
+      })
+    ).toMatchInlineSnapshot(`
+      {
+        "entries": {
+          "foo": {
+            "kind": "string",
+            "value": "foo",
+          },
+        },
+        "kind": "object",
+      }
+    `);
+  });
+
+  test("recursive named type", () => {
+    expect(
+      generateSerializableValue(namedType("/foo.tsx:Foo"), {
+        "/foo.tsx:Foo": {
+          type: objectType({
+            child: namedType("/foo.tsx:Bar"),
+          }),
+          parameters: {},
+        },
+
+        "/foo.tsx:Bar": {
+          type: objectType({
+            child: optionalType(namedType("/foo.tsx:Foo")),
+          }),
+          parameters: {},
+        },
+      })
+    ).toMatchInlineSnapshot(`
+      {
+        "entries": {
+          "child": {
+            "entries": {},
+            "kind": "object",
+          },
+        },
+        "kind": "object",
+      }
+    `);
+  });
+
+  test("recursive function type", () => {
+    expect(
+      generateSerializableValue(namedType("/foo.tsx:MyComponentProps"), {
+        "/foo.tsx:MyComponentProps": {
+          type: objectType({
+            foo: namedType("/foo.tsx:Fn"),
+          }),
+
+          parameters: {},
+        },
+
+        "/foo.tsx:Fn": {
+          type: functionType(namedType("/foo.tsx:Fn")),
+          parameters: {},
+        },
+      })
+    ).toMatchInlineSnapshot(`
+      {
+        "entries": {
+          "foo": {
+            "kind": "function",
+            "returnValue": {
+              "kind": "undefined",
+            },
+          },
+        },
+        "kind": "object",
+      }
+    `);
+  });
+
+  test("self-returning type", () => {
+    expect(
+      generateSerializableValue(namedType("/foo.tsx:User"), {
+        "/foo.tsx:User": {
+          type: objectType({
+            target: namedType("/foo.tsx:User"),
+            friends: functionType(arrayType(namedType("/foo.tsx:User"))),
+          }),
+
+          parameters: {},
+        },
+      })
+    ).toMatchInlineSnapshot(`
+      {
+        "entries": {
+          "friends": {
+            "kind": "function",
+            "returnValue": {
+              "items": [],
+              "kind": "array",
+            },
+          },
+        },
+        "kind": "object",
+      }
+    `);
+  });
+
+  test("all types", () => {
+    expect(
+      generateSerializableValue(namedType("/foo.tsx:Foo"), {
+        "/foo.tsx:Foo": {
+          type: objectType({
+            anyType: ANY_TYPE,
+            unknownType: UNKNOWN_TYPE,
+            neverType: NEVER_TYPE,
+            voidType: VOID_TYPE,
+            nullType: NULL_TYPE,
+            booleanType: BOOLEAN_TYPE,
+            stringType: STRING_TYPE,
+            numberType: NUMBER_TYPE,
+            reactNodeType: NODE_TYPE,
+            numberLiteral: literalType(123),
+            stringLiteral: literalType("foo"),
+            trueLiteral: literalType(true),
+            falseLiteral: literalType(false),
+            stringEnumType: enumType({
+              a: "A",
+              b: "B",
+              c: "C",
+            }),
+
+            numberEnumType: enumType({
+              a: 3,
+              b: 2,
+              c: 1,
+            }),
+
+            arrayType: arrayType(STRING_TYPE),
+            setType: setType(STRING_TYPE),
+            recordType: recordType(STRING_TYPE, NUMBER_TYPE),
+            unionType: unionType([STRING_TYPE, NUMBER_TYPE]),
+            intersectionType: intersectionType([STRING_TYPE, NUMBER_TYPE]),
+            functionType: functionType(STRING_TYPE),
+            promiseType: promiseType(STRING_TYPE),
+            namedType: namedType("/foo.tsx:Bar"),
+          }),
+
+          parameters: {},
+        },
+
+        "/foo.tsx:Bar": {
+          type: objectType({
+            bar: functionType(STRING_TYPE),
+          }),
+
+          parameters: {},
+        },
+      })
+    ).toMatchInlineSnapshot(`
+      {
+        "entries": {
+          "arrayType": {
+            "items": [
+              {
+                "kind": "string",
+                "value": "arrayType",
+              },
+            ],
+            "kind": "array",
+          },
+          "booleanType": {
+            "kind": "boolean",
+            "value": false,
+          },
+          "falseLiteral": {
+            "kind": "boolean",
+            "value": false,
+          },
+          "functionType": {
+            "kind": "function",
+            "returnValue": {
+              "kind": "string",
+              "value": "functionType",
+            },
+          },
+          "intersectionType": {
+            "kind": "string",
+            "value": "intersectionType",
+          },
+          "namedType": {
+            "entries": {
+              "bar": {
+                "kind": "function",
+                "returnValue": {
+                  "kind": "string",
+                  "value": "namedType.bar",
+                },
+              },
+            },
+            "kind": "object",
+          },
+          "nullType": {
+            "kind": "null",
+          },
+          "numberEnumType": {
+            "kind": "number",
+            "value": 3,
+          },
+          "numberLiteral": {
+            "kind": "number",
+            "value": 123,
+          },
+          "numberType": {
+            "kind": "number",
+            "value": 100,
+          },
+          "promiseType": {
+            "kind": "promise",
+            "value": {
+              "message": null,
+              "type": "reject",
+            },
+          },
+          "reactNodeType": {
+            "kind": "string",
+            "value": "reactNodeType",
+          },
+          "recordType": {
+            "entries": {},
+            "kind": "object",
+          },
+          "setType": {
+            "kind": "set",
+            "values": {
+              "items": [
+                {
+                  "kind": "string",
+                  "value": "setType",
+                },
+              ],
+              "kind": "array",
+            },
+          },
+          "stringEnumType": {
+            "kind": "string",
+            "value": "A",
+          },
+          "stringLiteral": {
+            "kind": "string",
+            "value": "foo",
+          },
+          "stringType": {
+            "kind": "string",
+            "value": "stringType",
+          },
+          "trueLiteral": {
+            "kind": "boolean",
+            "value": true,
+          },
+          "unionType": {
+            "kind": "string",
+            "value": "unionType",
+          },
+        },
+        "kind": "object",
+      }
+    `);
+  });
+
+  test("optional props in object type", () => {
+    expect(
+      generateSerializableValue(namedType("/foo.tsx:Foo"), {
+        "/foo.tsx:Foo": {
+          type: objectType({
+            a: STRING_TYPE,
+            b: optionalType(STRING_TYPE),
+            c: ANY_TYPE,
+            d: UNKNOWN_TYPE,
+            e: STRING_TYPE,
+            f: optionalType(STRING_TYPE),
+          }),
+
+          parameters: {},
+        },
+      })
+    ).toMatchInlineSnapshot(`
+      {
+        "entries": {
+          "a": {
+            "kind": "string",
+            "value": "a",
+          },
+          "e": {
+            "kind": "string",
+            "value": "e",
+          },
+        },
+        "kind": "object",
+      }
+    `);
+  });
+
+  test("missing named type", () => {
+    expect(
+      generateSerializableValue(namedType("/foo.tsx:Foo"), {
+        "/foo.tsx:Foo": {
+          type: objectType({
+            a: namedType("/foo.tsx:Missing"),
+          }),
+
+          parameters: {},
+        },
+      })
+    ).toMatchInlineSnapshot(`
+      {
+        "entries": {},
+        "kind": "object",
+      }
+    `);
+  });
+
+  test("generic types", () => {
+    expect(
+      generateSerializableValue(namedType("/foo.tsx:A"), {
+        "/foo.tsx:A": {
+          type: namedType("/foo.tsx:C", [namedType("T")]),
+          parameters: { T: NUMBER_TYPE },
+        },
+
+        "/foo.tsx:B": {
+          type: objectType({
+            foo: namedType("/foo.tsx:C", [namedType("/foo.tsx:B")]),
+          }),
+
+          parameters: {},
+        },
+
+        "/foo.tsx:C": {
+          type: objectType({
+            t: namedType("T"),
+            s: namedType("S"),
+          }),
+
+          parameters: {
+            T: null,
+            S: namedType("T"),
+          },
+        },
+      })
+    ).toMatchInlineSnapshot(`
+      {
+        "entries": {
+          "s": {
+            "kind": "number",
+            "value": 100,
+          },
+          "t": {
+            "kind": "number",
+            "value": 100,
+          },
+        },
+        "kind": "object",
+      }
+    `);
+  });
+});
