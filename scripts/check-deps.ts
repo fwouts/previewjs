1fbf510107662111619840217be8c23283a9d92a
François Wouts <f@zenc.io>
2022-07-03 21:56:40 +1000

    chore: set up dependencies checks (#742)

---
+++ scripts/check-deps.ts
@@ -0,0 +1,75 @@
+import depcheck from "depcheck";
+import execa from "execa";
+import path from "path";
+import { inspect } from "util";
+
+const globalIgnores = ["autoprefixer", "postcss", "tailwindcss", "unbuild"];
+
+// TODO: Go through these deps and eliminate the ones that are not needed.
+const localIgnores: Record<string, string[]> = {
+  "/": ["npm", "prettier", "turbo"],
+  "/core": ["monaco-editor"],
+  "/frameworks/react": ["@types/prop-types"],
+  "/frameworks/react/preview": ["@types/prop-types", "react", "react-dom"],
+  "/frameworks/vue2": ["vue"],
+  "/frameworks/vue3/preview": [
+    "@vue/shared",
+    "@vue/reactivity",
+    "@vue/runtime-core",
+    "@vue/runtime-dom",
+  ],
+  "/integrations/vscode": ["ovsx", "vscode"],
+};
+
+async function main() {
+  const rootDir = path.join(__dirname, "..");
+  const json = execa.commandSync("pnpm -r la --json").stdout;
+  const workspaces = JSON.parse(json);
+
+  let foundErrors = false;
+  for (const workspace of workspaces) {
+    const { path: workspacePath } = workspace;
+    const relativePath = "/" + path.relative(rootDir, workspacePath);
+    if (
+      relativePath.startsWith("/smoke-test-apps") ||
+      relativePath === "/dev-workspace"
+    ) {
+      continue;
+    }
+    const results = await depcheck(workspacePath, {
+      ignoreDirs: ["node_modules", "client/dist", "dist"],
+      ignoreMatches: [...globalIgnores, ...(localIgnores[relativePath] || [])],
+    });
+    if (results.dependencies.length > 0) {
+      console.error(
+        `Unwanted runtime dependencies from ${relativePath}: ${inspect(
+          results.dependencies
+        )}\n`
+      );
+      foundErrors = true;
+    }
+    if (results.devDependencies.length > 0) {
+      console.error(
+        `Unwanted dev dependencies from ${relativePath}: ${inspect(
+          results.devDependencies
+        )}\n`
+      );
+      foundErrors = true;
+    }
+    if (Object.keys(results.missing).length > 0) {
+      console.error(
+        `Missing packages from ${relativePath}: ${inspect(results.missing)}\n`
+      );
+      foundErrors = true;
+    }
+  }
+
+  if (!foundErrors) {
+    console.log("Success! No unwanted dependencies found.");
+  }
+}
+
+main().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
