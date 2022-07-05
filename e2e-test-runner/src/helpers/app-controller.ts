3054e2a62bd64d8a965e0a83838101f392b18ab4
François Wouts <f@zenc.io>
2022-07-05 15:29:05 +1000

    Update app-controller.ts

--- e2e-test-runner/src/helpers/app-controller.ts
+++ e2e-test-runner/src/helpers/app-controller.ts
@@ -26,10 +26,10 @@ export class AppController {
   async waitForIdle() {
     await this.page.waitForLoadState("networkidle");
     try {
-      (await this.previewIframe()).waitForLoadState("networkidle");
+      await (await this.previewIframe()).waitForLoadState("networkidle");
     } catch (e) {
       // It's OK for the iframe to be replace by another one, in which case wait again.
-      (await this.previewIframe()).waitForLoadState("networkidle");
+      await (await this.previewIframe()).waitForLoadState("networkidle");
     }
   }
 
