97023072387e1406650483e56406f7b34bf14a3d
François Wouts <f@zenc.io>
2022-07-15 16:07:55 +1000

    feat(serializable-values): add possible source to unknown values

--- serializable-values/src/serializable-value.ts
+++ serializable-values/src/serializable-value.ts
@@ -161,6 +161,16 @@ export const UNDEFINED = { kind: "undefined" } as const;
 
 export type SerializableUndefinedValue = typeof UNDEFINED;
 
-export const UNKNOWN = { kind: "unknown" } as const;
+export type SerializableUnknownValue = {
+  kind: "unknown";
+  source?: string;
+};
 
-export type SerializableUnknownValue = typeof UNKNOWN;
+export function unknown(source?: string): SerializableUnknownValue {
+  return {
+    kind: "unknown",
+    source,
+  };
+}
+
+export const UNKNOWN = unknown();
