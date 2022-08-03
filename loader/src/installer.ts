6372b8c914b522c73357065afc3a1e57f11d7b32
François Wouts <f@zenc.io>
2022-08-03 14:39:40 +1000

    Extract

--- loader/src/installer.ts
+++ loader/src/installer.ts
@@ -1,6 +1,8 @@
 import execa from "execa";
 import { mkdir, writeFile } from "fs-extra";
 import path from "path";
+import { checkNodeVersion } from "./checkNodeVersion";
+import { checkNpmVersion } from "./checkNpmVersion";
 import { loadModules } from "./modules";
 import packageLockJson from "./release/package-lock.json";
 import packageJson from "./release/package.json";
@@ -81,51 +83,5 @@ export async function install(options: {
   } catch (e) {
     options.onOutput(`\nOh no, it looks like installation failed!\n\n${e}`);
     throw e;
-  }
-}
-
-async function checkNodeVersion(cwd: string) {
-  const nodeVersionProcess = await execa("node", ["-v"], {
-    cwd,
-    reject: false,
-  });
-  if (nodeVersionProcess.failed) {
-    throw new Error(
-      `Preview.js was unable to run node.\n\nIs it installed? You may need to restart your IDE.`
-    );
-  }
-  if (nodeVersionProcess.exitCode !== 0) {
-    throw new Error(
-      `Preview.js was unable to run node (exit code ${nodeVersionProcess.exitCode}):\n\n${nodeVersionProcess.stderr}`
-    );
-  }
-  const nodeVersion = nodeVersionProcess.stdout;
-  if (parseInt(nodeVersion) < 14) {
-    throw new Error(
-      `Preview.js needs NodeJS 14+ to run, but current version is: ${nodeVersion}\n\nPlease upgrade then restart your IDE.`
-    );
-  }
-}
-
-async function checkNpmVersion(cwd: string) {
-  const npmVersionProcess = await execa("npm", ["-v"], {
-    cwd,
-    reject: false,
-  });
-  if (npmVersionProcess.failed) {
-    throw new Error(
-      `Preview.js was unable to run npm.\n\nYou can manually run "npm install" in ${cwd}\n\nYou will need to restart your IDE after doing so.`
-    );
-  }
-  if (npmVersionProcess.exitCode !== 0) {
-    throw new Error(
-      `Preview.js was unable to run npm (exit code ${npmVersionProcess.exitCode}):\n\n${npmVersionProcess.stderr}\n\nYou can manually run "npm install" in ${options.installDir}\n\nYou will need to restart your IDE after doing so.`
-    );
-  }
-  const npmVersion = npmVersionProcess.stdout;
-  if (parseInt(npmVersion) < 6) {
-    throw new Error(
-      `Preview.js needs npm 6+ to run, but current version is: ${npmVersion}\n\nPlease upgrade then restart your IDE.`
-    );
   }
 }
