bf230603b1cd43d09b09a6d7a050d67cd9ab61a6
François Wouts <f@zenc.io>
2022-08-03 14:36:13 +1000

    fix: crash with a helpful error message when Node 12 or earlier is detected

--- loader/src/installer.ts
+++ loader/src/installer.ts
@@ -44,26 +44,8 @@ export async function install(options: {
     "utf8"
   );
   try {
-    const npmVersionProcess = await execa("npm", ["-v"], {
-      cwd: options.installDir,
-      reject: false,
-    });
-    if (npmVersionProcess.failed) {
-      throw new Error(
-        `Preview.js was unable to run npm.\n\nYou can manually run "npm install" in ${options.installDir}\n\nYou will need to restart your IDE after doing so.`
-      );
-    }
-    if (npmVersionProcess.exitCode !== 0) {
-      throw new Error(
-        `Preview.js was unable to run npm (exit code ${npmVersionProcess.exitCode}):\n\n${npmVersionProcess.stderr}\n\nYou can manually run "npm install" in ${options.installDir}\n\nYou will need to restart your IDE after doing so.`
-      );
-    }
-    const version = npmVersionProcess.stdout;
-    if (parseInt(version) < 6) {
-      throw new Error(
-        `Preview.js needs npm 6+ to run, but current version is: ${version}\n\nPlease upgrade then restart your IDE.`
-      );
-    }
+    await checkNodeVersion(options.installDir);
+    await checkNpmVersion(options.installDir);
   } catch (e) {
     options.onOutput(`${e}`);
     throw e;
@@ -99,5 +81,51 @@ export async function install(options: {
   } catch (e) {
     options.onOutput(`\nOh no, it looks like installation failed!\n\n${e}`);
     throw e;
+  }
+}
+
+async function checkNodeVersion(cwd: string) {
+  const nodeVersionProcess = await execa("node", ["-v"], {
+    cwd,
+    reject: false,
+  });
+  if (nodeVersionProcess.failed) {
+    throw new Error(
+      `Preview.js was unable to run node.\n\nIs it installed? You may need to restart your IDE.`
+    );
+  }
+  if (nodeVersionProcess.exitCode !== 0) {
+    throw new Error(
+      `Preview.js was unable to run node (exit code ${nodeVersionProcess.exitCode}):\n\n${nodeVersionProcess.stderr}`
+    );
+  }
+  const nodeVersion = nodeVersionProcess.stdout;
+  if (parseInt(nodeVersion) < 14) {
+    throw new Error(
+      `Preview.js needs NodeJS 14+ to run, but current version is: ${nodeVersion}\n\nPlease upgrade then restart your IDE.`
+    );
+  }
+}
+
+async function checkNpmVersion(cwd: string) {
+  const npmVersionProcess = await execa("npm", ["-v"], {
+    cwd,
+    reject: false,
+  });
+  if (npmVersionProcess.failed) {
+    throw new Error(
+      `Preview.js was unable to run npm.\n\nYou can manually run "npm install" in ${cwd}\n\nYou will need to restart your IDE after doing so.`
+    );
+  }
+  if (npmVersionProcess.exitCode !== 0) {
+    throw new Error(
+      `Preview.js was unable to run npm (exit code ${npmVersionProcess.exitCode}):\n\n${npmVersionProcess.stderr}\n\nYou can manually run "npm install" in ${options.installDir}\n\nYou will need to restart your IDE after doing so.`
+    );
+  }
+  const npmVersion = npmVersionProcess.stdout;
+  if (parseInt(npmVersion) < 6) {
+    throw new Error(
+      `Preview.js needs npm 6+ to run, but current version is: ${npmVersion}\n\nPlease upgrade then restart your IDE.`
+    );
   }
 }
