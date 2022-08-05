cc89a9e25c733260b680db94f2b5d1bf7d1699b8
François Wouts <f@zenc.io>
2022-08-05 10:09:56 +1000

    test: enable e2e tests to be skipped individually

--- e2e-test-runner/src/test-case.ts
+++ e2e-test-runner/src/test-case.ts
@@ -1,5 +1,6 @@
 import type { FrameworkPluginFactory } from "@previewjs/core";
 import callerCallsite from "caller-callsite";
+import chalk from "chalk";
 import path from "path";
 import type { AppController } from "./helpers/app-controller";
 import type { AppDir } from "./test-runner";
@@ -35,14 +36,22 @@ export async function testSuite(
     throw new Error(`Unable to detect caller file path`);
   }
   const testCases: TestCase[] = [];
-  await testFactory((description, testAppName, run) => {
+  const test: TestCreator = ((
+    description: string,
+    testAppName: string,
+    run: RunTest
+  ) => {
     const testDir = path.join(__dirname, "..", "..", "test-apps", testAppName);
     testCases.push({
       description,
       testDir,
       run,
     });
-  });
+  }) as any;
+  test.skip = (description) => {
+    console.warn(chalk.yellow(`Skipping test: ${description}`));
+  };
+  await testFactory(test);
   return {
     frameworkPluginFactories,
     absoluteFilePath,
@@ -52,8 +61,12 @@ export async function testSuite(
   };
 }
 
-export type TestCreator = (
-  description: string,
-  testAppName: string,
-  run: (options: { appDir: AppDir; controller: AppController }) => Promise<void>
-) => void;
+export type TestCreator = {
+  (description: string, testAppName: string, run: RunTest): void;
+  skip(description: string, testAppName: string, run: RunTest): void;
+};
+
+type RunTest = (options: {
+  appDir: AppDir;
+  controller: AppController;
+}) => Promise<void>;
