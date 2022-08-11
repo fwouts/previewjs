f1b6f28b770460def89bb7306b29dd3ba2c1f2cc
François Wouts <f@zenc.io>
2022-08-11 11:30:12 +1000

    fix

--- core/copy_monaco_editor.sh
+++ core/copy_monaco_editor.sh
@@ -9,11 +9,11 @@ mkdir -p dist/monaco-editor/min/vs/base/worker
 mkdir -p dist/monaco-editor/min/vs/language/typescript
 
 pnpm shx cp node_modules/monaco-editor/LICENSE dist/monaco-editor/LICENSE
-pnpm shx cp node_modules/monaco-editor/min/vs/loader.js dist/monaco-editor/min/vs/loader.js
+pnpm shx cp node_modules/monaco-editor/min/vs/base dist/monaco-editor/min/vs/base
+pnpm shx cp node_modules/monaco-editor/min/vs/basic-languages/typescript/typescript.js dist/monaco-editor/min/vs/basic-languages/typescript/typescript.js
 pnpm shx cp node_modules/monaco-editor/min/vs/editor/editor.main.js dist/monaco-editor/min/vs/editor/editor.main.js
 pnpm shx cp node_modules/monaco-editor/min/vs/editor/editor.main.css dist/monaco-editor/min/vs/editor/editor.main.css
 pnpm shx cp node_modules/monaco-editor/min/vs/editor/editor.main.nls.js dist/monaco-editor/min/vs/editor/editor.main.nls.js
-pnpm shx cp node_modules/monaco-editor/min/vs/basic-languages/typescript/typescript.js dist/monaco-editor/min/vs/basic-languages/typescript/typescript.js
 pnpm shx cp node_modules/monaco-editor/min/vs/language/typescript/tsMode.js dist/monaco-editor/min/vs/language/typescript/tsMode.js
-pnpm shx cp node_modules/monaco-editor/min/vs/base/worker/workerMain.js dist/monaco-editor/min/vs/base/worker/workerMain.js
 pnpm shx cp node_modules/monaco-editor/min/vs/language/typescript/tsWorker.js dist/monaco-editor/min/vs/language/typescript/tsWorker.js
+pnpm shx cp node_modules/monaco-editor/min/vs/loader.js dist/monaco-editor/min/vs/loader.js
