bd6b63a4520f5be7e9896180ef5b7de328ebdfa8
François Wouts <f@zenc.io>
2022-08-11 11:30:49 +1000

    Update copy_monaco_editor.sh

--- core/copy_monaco_editor.sh
+++ core/copy_monaco_editor.sh
@@ -9,7 +9,7 @@ mkdir -p dist/monaco-editor/min/vs/base/worker
 mkdir -p dist/monaco-editor/min/vs/language/typescript
 
 pnpm shx cp node_modules/monaco-editor/LICENSE dist/monaco-editor/LICENSE
-pnpm shx cp node_modules/monaco-editor/min/vs/base dist/monaco-editor/min/vs/base
+pnpm shx cp -r node_modules/monaco-editor/min/vs/base dist/monaco-editor/min/vs/base
 pnpm shx cp node_modules/monaco-editor/min/vs/basic-languages/typescript/typescript.js dist/monaco-editor/min/vs/basic-languages/typescript/typescript.js
 pnpm shx cp node_modules/monaco-editor/min/vs/editor/editor.main.js dist/monaco-editor/min/vs/editor/editor.main.js
 pnpm shx cp node_modules/monaco-editor/min/vs/editor/editor.main.css dist/monaco-editor/min/vs/editor/editor.main.css
