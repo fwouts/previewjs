6580b5bb42fe37592837d6746d842aab61fa30bd
François Wouts <f@zenc.io>
2022-08-12 16:11:16 +1000

    chore: set up new release workflow

---
+++ scripts/release-apps.ts
@@ -0,0 +1,183 @@
+import execa from "execa";
+import fs from "fs";
+import { readFile, realpath } from "fs/promises";
+import inquirer from "inquirer";
+import path from "path";
+import { previewjsProVersion } from "../loader/src/version";
+import { gitChangelog } from "./git-changelog";
+import { incrementVersion } from "./increment-version";
+import { getPackageJson } from "./package-json";
+
+async function main() {
+  await assertCleanGit();
+
+  console.log(
+    `About to update loader bundle with @previewjs/pro v${previewjsProVersion}.`
+  );
+  const { version: coreVersion } = await import("../core/package.json");
+  const { version: vfsVersion } = await import("../vfs/package.json");
+  const { version: reactPluginVersion } = await import(
+    "../frameworks/react/package.json"
+  );
+  const { version: solidPluginVersion } = await import(
+    "../frameworks/solid/package.json"
+  );
+  const { version: vue2PluginVersion } = await import(
+    "../frameworks/vue2/package.json"
+  );
+  const { version: vue3PluginVersion } = await import(
+    "../frameworks/vue3/package.json"
+  );
+  const esbuildPackageJsonPath = await realpath(
+    `${__dirname}/../core/node_modules/vite/../esbuild/package.json`
+  );
+  const esbuildPackageJson = await readFile(esbuildPackageJsonPath, "utf8");
+  const { optionalDependencies: esbuildOptionalDependencies } =
+    JSON.parse(esbuildPackageJson);
+  const esbuildBinaryPackages = [
+    "esbuild-darwin-64",
+    "esbuild-darwin-arm64",
+    "esbuild-linux-64",
+    "esbuild-linux-arm64",
+    "esbuild-windows-32",
+    "esbuild-windows-64",
+    "esbuild-windows-arm64",
+  ];
+  const esbuildBinaryDependencies: Record<string, string> = {};
+  for (const binaryPackage of esbuildBinaryPackages) {
+    const version = esbuildOptionalDependencies[binaryPackage];
+    if (!version) {
+      throw new Error(
+        `Missing esbuild binary dependency: ${binaryPackage}. Perhaps the release script needs to be updated.`
+      );
+    }
+    esbuildBinaryDependencies[binaryPackage] = version;
+  }
+  const releaseDirPath = path.join(__dirname, "..", "loader", "src", "release");
+  await fs.promises.writeFile(
+    path.join(releaseDirPath, "package.json"),
+    JSON.stringify(
+      {
+        dependencies: {
+          "@previewjs/core": coreVersion,
+          "@previewjs/plugin-react": reactPluginVersion,
+          "@previewjs/plugin-solid": solidPluginVersion,
+          "@previewjs/plugin-vue2": vue2PluginVersion,
+          "@previewjs/plugin-vue3": vue3PluginVersion,
+          "@previewjs/pro": previewjsProVersion,
+          "@previewjs/vfs": vfsVersion,
+          ...esbuildBinaryDependencies,
+        },
+      },
+      null,
+      2
+    ),
+    "utf8"
+  );
+  console.log(`Running npm install to fetch bundle and update lockfile...`);
+  await execa("pnpm", ["npm", "install", "--ignore-scripts", "-f"], {
+    cwd: releaseDirPath,
+  });
+  const prompt = inquirer.createPromptModule();
+  const { releaseIntellij } = await prompt({
+    name: "releaseIntellij",
+    type: "confirm",
+    message: "Release IntelliJ plugin?",
+  });
+  if (releaseIntellij) {
+    await releaseIntellijPlugin();
+  }
+  const { releaseVscode } = await prompt({
+    name: "releaseVscode",
+    type: "confirm",
+    message: "Release VS Code extension?",
+  });
+  if (releaseVscode) {
+    await releaseVscodeExtension();
+  }
+  const { releaseCli } = await prompt({
+    name: "releaseCli",
+    type: "confirm",
+    message: "Release CLI?",
+  });
+  if (releaseCli) {
+    await releaseCliApp();
+  }
+}
+
+async function releaseIntellijPlugin() {
+  const intellijPath = path.join(__dirname, "integrations", "intellij");
+  const gradlePropertiesPath = `${intellijPath}/gradle.properties`;
+  const gradlePropertiesContent = await fs.promises.readFile(
+    gradlePropertiesPath,
+    "utf8"
+  );
+  const oldVersion = gradlePropertiesContent
+    .split("\n")
+    .find((line) => line.startsWith("pluginVersion = "))
+    ?.split(" = ")[1];
+  if (!oldVersion) {
+    throw new Error(`Unable to find version in ${gradlePropertiesPath}`);
+  }
+  const version = await incrementVersion(oldVersion);
+  await fs.promises.writeFile(
+    gradlePropertiesPath,
+    gradlePropertiesContent
+      .split("\n")
+      .map((line) =>
+        line.startsWith("pluginVersion = ")
+          ? `pluginVersion = ${version}`
+          : line
+      )
+      .join("\n"),
+    "utf8"
+  );
+  await release("integrations/intellij", version);
+}
+
+async function releaseVscodeExtension() {
+  const vscodePath = path.join(__dirname, "integrations", "vscode");
+  const packageJson = getPackageJson(`${vscodePath}/package.json`);
+  const { version: currentVersion } = await packageJson.read();
+  const version = await incrementVersion(currentVersion);
+  await packageJson.updateVersion(version);
+  await fs.promises.writeFile(
+    packageJson.absoluteFilePath,
+    (
+      await fs.promises.readFile(packageJson.absoluteFilePath, "utf8")
+    ).replace(/-\d+\.\d+\.\d+\.vsix/g, `-${version}.vsix`),
+    "utf8"
+  );
+  await release("integrations/vscode", version);
+}
+
+async function releaseCliApp() {
+  const cliPath = path.join(__dirname, "cli");
+  const packageJson = getPackageJson(`${cliPath}/package.json`);
+  const { version: currentVersion } = await packageJson.read();
+  const version = await incrementVersion(currentVersion);
+  await packageJson.updateVersion(version);
+  await release("cli", version);
+}
+
+async function release(packageName: string, version: string) {
+  const changelog = await gitChangelog(packageName, ["."]);
+  const tag = `${packageName}/v${version}`;
+  console.log(`The following tag will be created: ${tag}\n`);
+  console.log(`Running pnpm install...`);
+  await execa("pnpm", ["install"]);
+  await execa("git", ["add", "."]);
+  await execa("git", ["commit", "-m", `release: ${packageName}@${version}`]);
+  await execa("git", ["tag", "-a", tag, "-m", ""]);
+  console.log(`Pushing commit...`);
+  await execa("git", ["push", "origin", "main"]);
+  await execa("git", ["push", "origin", tag]);
+  console.log(`Creating release...`);
+  await execa("gh", ["release", "create", tag, "-t", tag, "-n", changelog]);
+  console.log(`Done!`);
+}
+
+main().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
---
+++ scripts/release-components.ts
@@ -0,0 +1,214 @@
+import execa from "execa";
+import fs from "fs";
+import inquirer from "inquirer";
+import path from "path";
+import { inspect } from "util";
+import { assertCleanGit } from "./clean-git";
+import { gitChangelog } from "./git-changelog";
+import { incrementVersion } from "./increment-version";
+import { getPackageJson } from "./package-json";
+
+type Package = {
+  name: string;
+  dirPath: string;
+  additionalChangelogPath?: string[];
+  ignoreDeps?: string[];
+  tagName: string;
+};
+
+const packages: Package[] = [
+  {
+    name: "api",
+    dirPath: "api",
+    tagName: "api",
+  },
+  {
+    name: "app",
+    dirPath: "app",
+    tagName: "app",
+  },
+  {
+    name: "app-foundations",
+    dirPath: "app-foundations",
+    tagName: "app-foundations",
+  },
+  {
+    name: "core",
+    dirPath: "core",
+    tagName: "core",
+  },
+  {
+    name: "config",
+    dirPath: "config",
+    tagName: "config",
+  },
+  {
+    name: "csf3",
+    dirPath: "csf3",
+    tagName: "csf3",
+  },
+  {
+    name: "e2e-test-runner",
+    dirPath: "e2e-test-runner",
+    tagName: "e2e-test-runner",
+  },
+  {
+    name: "iframe",
+    dirPath: "iframe",
+    tagName: "iframe",
+  },
+  {
+    name: "plugin-react",
+    dirPath: "frameworks/react",
+    tagName: "plugins/react",
+  },
+  {
+    name: "plugin-solid",
+    dirPath: "frameworks/solid",
+    tagName: "plugins/solid",
+  },
+  {
+    name: "plugin-vue2",
+    dirPath: "frameworks/vue2",
+    tagName: "plugins/vue2",
+  },
+  {
+    name: "plugin-vue3",
+    dirPath: "frameworks/vue3",
+    tagName: "plugins/vue3",
+  },
+  {
+    name: "serializable-values",
+    dirPath: "serializable-values",
+    tagName: "serializable-values",
+  },
+  {
+    name: "type-analyzer",
+    dirPath: "type-analyzer",
+    tagName: "type-analyzer",
+  },
+  {
+    name: "vfs",
+    dirPath: "vfs",
+    tagName: "vfs",
+  },
+];
+
+async function main() {
+  await assertCleanGit();
+
+  const localDependencies: Record<string, Set<string>> = {};
+  for (const packageInfo of packages) {
+    const deps = new Set<string>();
+    const packageJson = JSON.parse(
+      await fs.promises.readFile(
+        path.join(packageInfo.dirPath, "package.json"),
+        "utf8"
+      )
+    );
+    for (const packageName of Object.keys({
+      ...packageJson.dependencies,
+      ...packageJson.devDependencies,
+    })) {
+      const [org, scopedName] = packageName.split("/");
+      if (org !== "@previewjs") {
+        continue;
+      }
+      if (!scopedName) {
+        throw new Error(`Expected a scoped package, found ${packageName}`);
+      }
+      if (!packageInfo.ignoreDeps?.includes(scopedName)) {
+        deps.add(scopedName);
+      }
+    }
+    localDependencies[packageInfo.name] = deps;
+  }
+
+  const releasedPackages: string[] = [];
+  while (Object.entries(localDependencies).length > 0) {
+    const nextPackageWithoutDeps = Object.entries(localDependencies).find(
+      ([_, deps]) => deps.size === 0
+    );
+    if (!nextPackageWithoutDeps) {
+      throw new Error(
+        `Invalid state: no package left without updated dependencies.\n\nRemaining:\n${inspect(
+          localDependencies
+        )}`
+      );
+    }
+    const scopedName = nextPackageWithoutDeps[0];
+    const packageInfo = packages.find((p) => p.name === scopedName);
+    if (!packageInfo) {
+      throw new Error(`Missing package info for name: ${scopedName}`);
+    }
+    const dependents = Object.entries(localDependencies)
+      .filter(([_, deps]) => deps.has(scopedName))
+      .map(([name]) => name);
+    const released = await preparePackageRelease(packageInfo, dependents);
+    if (released) {
+      releasedPackages.push(released);
+    }
+    for (const deps of Object.values(localDependencies)) {
+      deps.delete(scopedName);
+    }
+    delete localDependencies[scopedName];
+  }
+
+  if (releasedPackages.length > 0) {
+    await execa("pnpm", ["install"]);
+    await execa("git", ["add", "."]);
+    await execa("git", [
+      "commit",
+      "-m",
+      `release: ${releasedPackages.join(", ")}`,
+    ]);
+    await execa("git", ["push", "origin", "main"]);
+  }
+}
+
+async function preparePackageRelease(
+  packageInfo: Package,
+  dependents: string[]
+): Promise<string | null> {
+  const packageName = `@previewjs/${packageInfo.name}`;
+  const packageJson = getPackageJson(`${packageInfo.dirPath}/package.json`);
+  const { version: currentVersion } = await packageJson.read();
+  console.log(
+    `About to release: ${packageName} (current version: ${currentVersion})`
+  );
+  const changelog = await gitChangelog(packageName, [
+    packageInfo.dirPath,
+    ...(packageInfo.additionalChangelogPath || []),
+  ]);
+  if (!changelog) {
+    console.log(`There is nothing to release.\n`);
+    return null;
+  }
+  const prompt = inquirer.createPromptModule();
+  console.log(`You are about to release the following:\n${changelog}`);
+  const { shouldRelease } = await prompt({
+    name: "shouldRelease",
+    type: "confirm",
+    message: `Would you like to release this?`,
+  });
+  if (!shouldRelease) {
+    return null;
+  }
+  const version = await incrementVersion(currentVersion);
+  await packageJson.updateVersion(version);
+  for (const dependent of dependents) {
+    const depPackageInfo = packages.find((p) => p.name === dependent);
+    if (!depPackageInfo) {
+      throw new Error(`Unable to find package info for: ${dependent}`);
+    }
+    await getPackageJson(
+      `${depPackageInfo.dirPath}/package.json`
+    ).updateDependency(packageName, version);
+  }
+  return `${packageName}@v${version}`;
+}
+
+main().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
