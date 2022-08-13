f1716a8deb9915b26b70cd048a71ff476d65f75d
François Wouts <f@zenc.io>
2022-08-14 00:18:54 +1000

    Revert "hack to make Rollup not complain about virtual index.html file name generated by Vite"
    This reverts commit 53ee7679034e2c71d6202922fec59f1f4f02f49f.

--- core/src/vite/plugins/virtual-plugin.ts
+++ core/src/vite/plugins/virtual-plugin.ts
@@ -4,6 +4,8 @@ import path from "path";
 import type * as vite from "vite";
 import { transformWithEsbuild } from "vite";
 
+const VIRTUAL_PREFIX = `/@previewjs-virtual:`;
+
 const jsExtensions = new Set([".js", ".jsx", ".ts", ".tsx"]);
 
 export function virtualPlugin(options: {
@@ -16,8 +18,12 @@ export function virtualPlugin(options: {
   return {
     name: "previewjs:virtual-fs",
     resolveId: async function (id, importer) {
+      const virtualImporter = importer?.startsWith(VIRTUAL_PREFIX) || false;
       if (id.indexOf(`/node_modules/`) !== -1) {
         return null;
+      }
+      if (id.startsWith(VIRTUAL_PREFIX)) {
+        id = id.slice(VIRTUAL_PREFIX.length);
       }
       const extension = path.extname(id);
       let absoluteId;
@@ -26,6 +32,9 @@ export function virtualPlugin(options: {
       } else {
         if (!importer || importer.indexOf(`/node_modules/`) !== -1) {
           return null;
+        }
+        if (virtualImporter) {
+          importer = importer.slice(VIRTUAL_PREFIX.length);
         }
         if (extension && !jsExtensions.has(extension) && extension !== ".svg") {
           // Virtual files mess with CSS processors like postcss.
@@ -36,14 +45,15 @@ export function virtualPlugin(options: {
       const resolved = await resolveAbsoluteModuleId(absoluteId);
       if (resolved) {
         const [absoluteFilePath] = resolved;
-        if (await fs.pathExists(absoluteFilePath)) {
+        if ((await fs.pathExists(absoluteFilePath)) && !virtualImporter) {
           // This file doesn't need to be virtual.
           return null;
         }
         if (!absoluteId.endsWith(extension)) {
           absoluteId += extension;
         }
-        const resolvedId = absoluteFilePath.replace(/\\/g, "/");
+        const resolvedId =
+          VIRTUAL_PREFIX + absoluteFilePath.replace(/\\/g, "/");
         return resolvedId;
       }
       return null;
@@ -51,6 +61,9 @@ export function virtualPlugin(options: {
     load: async function (id) {
       if (id.indexOf(`/node_modules/`) !== -1) {
         return null;
+      }
+      if (id.startsWith(VIRTUAL_PREFIX)) {
+        id = id.slice(VIRTUAL_PREFIX.length);
       }
       const resolved = await resolveAbsoluteModuleId(id);
       if (!resolved) {
@@ -97,7 +110,8 @@ export function virtualPlugin(options: {
         return;
       }
       // Note: backslash handling is Windows-specific.
-      const virtualModuleId = absoluteFilePath.replace(/\\/g, "/");
+      const virtualModuleId =
+        VIRTUAL_PREFIX + absoluteFilePath.replace(/\\/g, "/");
       const node = moduleGraph.getModuleById(virtualModuleId);
       return node && [node];
     },
