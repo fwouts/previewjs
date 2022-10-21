5a535dbae625ba777c6378a840f8ffcb5c5bf668
François Wouts <f@zenc.io>
2022-10-21 22:04:48 +1100

    Check in .svelte-kit instead of generating it (work around postinstall crash in CI)

---
+++ smoke-test-apps/sveltekit-app/.svelte-kit/types/src/routes/$types.d.ts
@@ -0,0 +1,16 @@
+import type * as Kit from '@sveltejs/kit';
+
+type Expand<T> = T extends infer O ? { [K in keyof O]: O[K] } : never;
+type RouteParams = {  }
+type MaybeWithVoid<T> = {} extends T ? T | void : T;
+export type RequiredKeys<T> = { [K in keyof T]-?: {} extends { [P in K]: T[K] } ? never : K; }[keyof T];
+type OutputDataShape<T> = MaybeWithVoid<Omit<App.PageData, RequiredKeys<T>> & Partial<Pick<App.PageData, keyof T & keyof App.PageData>> & Record<string, any>>
+type EnsureDefined<T> = T extends null | undefined ? {} : T;
+type PageParentData = EnsureDefined<LayoutData>;
+type LayoutParams = RouteParams & {  }
+type LayoutParentData = EnsureDefined<{}>;
+
+export type PageServerData = null;
+export type PageData = Expand<PageParentData>;
+export type LayoutServerData = null;
+export type LayoutData = Expand<LayoutParentData>;
