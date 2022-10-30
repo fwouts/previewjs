4dfb3ebb6bc5d6f206b200db6b0d768cd4ca700b
François Wouts <f@zenc.io>
2022-10-31 10:50:04 +1100

    Update UpdateBannerState.ts

--- app-foundations/src/components/UpdateBanner/UpdateBannerState.ts
+++ app-foundations/src/components/UpdateBanner/UpdateBannerState.ts
@@ -1,4 +1,4 @@
-import type { localEndpoints, ResponseOf } from "@previewjs/api";
+import type { localRPCs, ResponseOf } from "@previewjs/api";
 import axios from "axios";
 import { makeAutoObservable, runInAction } from "mobx";
 import type { PersistedStateController } from "../../state/PersistedStateController";
@@ -13,7 +13,7 @@ export class UpdateBannerState {
     makeAutoObservable(this);
   }
 
-  async start(appInfo: ResponseOf<typeof localEndpoints.GetInfo>["appInfo"]) {
+  async start(appInfo: ResponseOf<typeof localRPCs.GetInfo>["appInfo"]) {
     try {
       const request: CheckVersionRequest = {
         appInfo,
