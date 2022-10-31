b6f90c4ddd4d7630ebb346726445ec72b9ed4e18
François Wouts <f@zenc.io>
2022-10-31 12:57:20 +1100

    feat: introduce @previewjs/config-helper-nextjs to simplify Next.js configuration

--- smoke-test-apps/nextjs-12/pages/index.tsx
+++ smoke-test-apps/nextjs-12/pages/index.tsx
@@ -2,9 +2,14 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import styles from "../styles/Home.module.css";
 
 const App: NextPage = () => {
+  const router = useRouter();
+
+  console.log("Current path is", router.pathname);
+
   return (
     <div id="ready" className={styles.container}>
       <Head>
