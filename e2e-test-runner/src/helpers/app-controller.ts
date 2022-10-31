20c7703743c51976075774d3598588161db40730
François Wouts <f@zenc.io>
2022-10-31 13:14:33 +1100

    no logging!

--- e2e-test-runner/src/helpers/app-controller.ts
+++ e2e-test-runner/src/helpers/app-controller.ts
@@ -107,7 +107,6 @@ export class AppController {
     const matchingDetectedComponent = detectedComponents.find(
       (c) => componentId === `${filePath.replace(/\\/g, "/")}:${c.name}`
     );
-    console.error(detectedComponents, componentId);
     if (!matchingDetectedComponent && !options.expectMissing) {
       throw new Error(
         `Component may be previewable but was not detected by framework plugin: ${componentId}`
