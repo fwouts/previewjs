a2e9549417c7c34f9fcf4061bd8ad8735f5594d6
François Wouts <f@zenc.io>
2022-10-31 17:06:19 +1100

    revert svelte-kit changes

---
+++ smoke-test-apps/sveltekit-app/.svelte-kit/generated/client-manifest.js
@@ -0,0 +1,15 @@
+export { matchers } from './client-matchers.js';
+
+			export const nodes = [() => import('./nodes/0'),
+	() => import('./nodes/1'),
+	() => import('./nodes/2')];
+
+			export const server_loads = [];
+
+			export const dictionary = {
+	"": [2]
+};
+
+			export const hooks = {
+				handleError: (({ error }) => { console.error(error) }),
+			};
---
+++ smoke-test-apps/sveltekit-demo/.svelte-kit/types/src/routes/sverdle/$types.d.ts
@@ -0,0 +1,19 @@
+import type * as Kit from '@sveltejs/kit';
+
+type Expand<T> = T extends infer O ? { [K in keyof O]: O[K] } : never;
+type RouteParams = {  }
+type MaybeWithVoid<T> = {} extends T ? T | void : T;
+export type RequiredKeys<T> = { [K in keyof T]-?: {} extends { [P in K]: T[K] } ? never : K; }[keyof T];
+type OutputDataShape<T> = MaybeWithVoid<Omit<App.PageData, RequiredKeys<T>> & Partial<Pick<App.PageData, keyof T & keyof App.PageData>> & Record<string, any>>
+type EnsureDefined<T> = T extends null | undefined ? {} : T;
+type PageServerParentData = EnsureDefined<import('../$types.js').LayoutServerData>;
+type PageParentData = EnsureDefined<import('../$types.js').LayoutData>;
+
+export type PageServerLoad<OutputData extends OutputDataShape<PageServerParentData> = OutputDataShape<PageServerParentData>> = Kit.ServerLoad<RouteParams, PageServerParentData, OutputData>;
+export type PageServerLoadEvent = Parameters<PageServerLoad>[0];
+export type ActionData = Expand<Kit.AwaitedActions<typeof import('./proxy+page.server.js').actions>> | undefined;
+export type PageServerData = Expand<Kit.AwaitedProperties<Awaited<ReturnType<typeof import('./proxy+page.server.js').load>>>>;
+export type PageData = Expand<Omit<PageParentData, keyof PageServerData> & EnsureDefined<PageServerData>>;
+export type Action = Kit.Action<RouteParams>
+export type Actions = Kit.Actions<RouteParams>
+export type RequestEvent = Kit.RequestEvent<RouteParams>;
