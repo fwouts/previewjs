e7544e9ceb19bfd35b0c554fc9b2447c844e0f44
François Wouts <f@zenc.io>
2022-11-01 00:19:11 +1100

    Update

--- screenshot/src/index.ts
+++ screenshot/src/index.ts
@@ -1,3 +1,4 @@
+import { RPCs } from "@previewjs/api";
 import { createWorkspace, loadPreviewEnv } from "@previewjs/core";
 import reactFrameworkPlugin from "@previewjs/plugin-react";
 import solidFrameworkPlugin from "@previewjs/plugin-solid";
@@ -7,9 +8,7 @@ import vue3FrameworkPlugin from "@previewjs/plugin-vue3";
 import { createFileSystemReader } from "@previewjs/vfs";
 import express from "express";
 import fs from "fs";
-import glob from "glob";
 import path from "path";
-import { promisify } from "util";
 
 main().catch((e) => {
   console.error(e);
@@ -52,33 +51,9 @@ async function main() {
       `No workspace could be created for directory: ${rootDirPath}`
     );
   }
-  // TODO: Use core.analyzeProject().
-  const absoluteFilePaths = await findFiles(
-    rootDirPath,
-    "**/*.@(js|jsx|ts|tsx|svelte|vue)"
-  );
-  const found = await env.frameworkPlugin.detectComponents(
-    workspace.typeAnalyzer,
-    absoluteFilePaths
-  );
-  console.error(found);
+  const found = await workspace.localRpc(RPCs.DetectComponents, {});
+  console.log(found);
   await workspace.preview.start(async () => 3250);
-}
-
-async function findFiles(rootDirPath: string, pattern: string) {
-  const files = await promisify(glob)(pattern, {
-    ignore: ["**/node_modules/**"],
-    cwd: rootDirPath,
-    nodir: true,
-    absolute: true,
-    follow: false,
-  });
-  // Note: in some cases, presumably because of yarn using link
-  // for faster node_modules, glob may return files in the parent
-  // directory. We filter them out here.
-  return files.filter((f) =>
-    f.startsWith(rootDirPath.replace(/\\/g, "/") + "/")
-  );
 }
 
 function findClientDir(dirPath: string): string {
