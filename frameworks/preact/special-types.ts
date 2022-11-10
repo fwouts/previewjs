479730ae2d3b7828c9168b8ba54cb50a14a1f8ea
Klemen Slavič <krof.drakula@gmail.com>
2022-11-10 13:41:28 +0100

    Implement plugin default export

---
+++ frameworks/preact/special-types.ts
@@ -0,0 +1,16 @@
+import {
+  arrayType,
+  functionType,
+  NODE_TYPE,
+  ValueType,
+} from "@previewjs/type-analyzer";
+
+export const PREACT_SPECIAL_TYPES: Record<string, ValueType> = {
+  Component: NODE_TYPE,
+  ComponentClass: functionType(NODE_TYPE),
+  FunctionComponent: functionType(NODE_TYPE),
+  PreactElement: NODE_TYPE,
+  PreactNode: NODE_TYPE,
+  PreactNodeArray: arrayType(NODE_TYPE),
+  PreactPortal: NODE_TYPE,
+};
---
+++ frameworks/preact/src/index.ts
@@ -0,0 +1,71 @@
+import type { Component, FrameworkPluginFactory } from "@previewjs/core";
+import path from "path";
+import ts from "typescript";
+import { extractPreactComponents } from "./extract-component";
+import { PREACT_SPECIAL_TYPES } from "../special-types";
+
+/** @deprecated */
+export const preactFrameworkPlugin: FrameworkPluginFactory = {
+  isCompatible: async dependencies => {
+    const version = await dependencies["react"]?.readInstalledVersion();
+    if (!version) {
+      return false;
+    }
+    return parseInt(version) >= 16;
+  },
+  async create() {
+    const previewDirPath = path.resolve(__dirname, "..", "preview");
+    return {
+      pluginApiVersion: 3,
+      name: "@previewjs/plugin-react",
+      defaultWrapperPath: "__previewjs__/Wrapper.tsx",
+      previewDirPath,
+      specialTypes: PREACT_SPECIAL_TYPES,
+      tsCompilerOptions: {
+        jsx: ts.JsxEmit.ReactJSX,
+        jsxImportSource: "preact",
+      },
+      detectComponents: async (typeAnalyzer, absoluteFilePaths) => {
+        const resolver = typeAnalyzer.analyze(absoluteFilePaths);
+        const components: Component[] = [];
+        for (const absoluteFilePath of absoluteFilePaths) {
+          components.push(
+            ...extractPreactComponents(resolver, absoluteFilePath)
+          );
+        }
+        return components;
+      },
+      viteConfig: () => {
+        return {
+          resolve: {
+            alias: {
+              react: "preact/compat",
+              "react-dom/test-utils": "preact/test-utils",
+              "react-dom": "preact/compat",
+              "react/jsx-runtime": "preact/jsx-runtime",
+              "react-native": "preact/compat",
+            },
+          },
+          plugins: [
+            {
+              name: "previewjs:disable-preact-hmr",
+              async transform(code, id) {
+                if (!id.endsWith(".jsx") && !id.endsWith(".tsx")) {
+                  return null;
+                }
+                // HMR prevents preview props from being refreshed.
+                // For now, we disable it entirely.
+                return code.replace(/import\.meta/g, "({})");
+              },
+            },
+          ],
+          define: {
+            "process.env.RUNNING_INSIDE_PREVIEWJS": "1",
+          },
+        };
+      },
+    };
+  },
+};
+
+export default preactFrameworkPlugin;
