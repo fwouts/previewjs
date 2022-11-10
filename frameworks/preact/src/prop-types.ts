7cb64ab7b260494df0ca60bf8412345a1b4c0ac7
Klemen Slavič <krof.drakula@gmail.com>
2022-11-10 13:20:07 +0100

    WIP: adding prop type analysis for Preact components

--- frameworks/preact/src/prop-types.ts
+++ frameworks/preact/src/prop-types.ts
@@ -5,39 +5,20 @@ export function detectPropTypes(
   name: string
 ): ts.Expression | null {
   for (const statement of sourceFile.statements) {
-    if (
-      ts.isExpressionStatement(statement) &&
-      ts.isBinaryExpression(statement.expression) &&
-      statement.expression.operatorToken.kind === ts.SyntaxKind.EqualsToken &&
-      ts.isPropertyAccessExpression(statement.expression.left) &&
-      ts.isIdentifier(statement.expression.left.expression)
-    ) {
-      const componentName = statement.expression.left.expression.text;
-      if (componentName !== name) {
-        continue;
-      }
-
-      if (
-        ts.isFunctionDeclaration(statement.expression.right) ||
-        ts.isFunctionExpression(statement.expression.right)
-      ) {
-        const componentFunction = statement.expression.right;
-      }
-    } else if (ts.isClassDeclaration(statement) && statement.name) {
-      const componentName = statement.name.text;
-      if (componentName !== name) {
-        continue;
-      }
-      for (const classMember of statement.members) {
-        // Look for prop types members.
+    if (ts.isVariableStatement(statement)) {
+      for (const varDeclaration of statement.declarationList.declarations) {
+        if (varDeclaration.name.getText() != name) continue;
         if (
-          ts.isPropertyDeclaration(classMember) &&
-          classMember.initializer &&
-          classMember.name.getText() === "propTypes"
+          ts.isArrowFunction(varDeclaration.initializer) ||
+          ts.isFunctionExpression(varDeclaration.initializer)
         ) {
-          return classMember.initializer;
+          return (varDeclaration.initializer as ts.ArrowFunction).parameters[0]
+            .initializer;
         }
       }
+    } else if (ts.isFunctionDeclaration(statement)) {
+      if (statement.name.getText() != name) continue;
+      return statement.parameters[0].initializer;
     }
   }
   return null;
