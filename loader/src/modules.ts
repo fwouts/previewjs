f0eb398c3d8c6c0d081ef905bce29997a3932ef1
François Wouts <f@zenc.io>
2022-11-18 22:30:50 +1100

    refactor: migrate all remaining framework plugins to unbuild

--- loader/src/modules.ts
+++ loader/src/modules.ts
@@ -16,10 +16,10 @@ export function loadModules({
     requireModule(packageName);
   const frameworkPluginFactories: core.FrameworkPluginFactory[] = [
     requireModule("@previewjs/plugin-react"),
-    requireModule("@previewjs/plugin-solid").default,
-    requireModule("@previewjs/plugin-svelte").default,
-    requireModule("@previewjs/plugin-vue2").default,
-    requireModule("@previewjs/plugin-vue3").default,
+    requireModule("@previewjs/plugin-solid"),
+    requireModule("@previewjs/plugin-svelte"),
+    requireModule("@previewjs/plugin-vue2"),
+    requireModule("@previewjs/plugin-vue3"),
   ];
 
   function requireModule(name: string) {
