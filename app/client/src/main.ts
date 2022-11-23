efe83e9fe81b2b49d7d16e169e9e44930f2c3ffe
François Wouts <f@zenc.io>
2022-11-23 21:24:52 +1100

    Remove PreviewState class

--- app/client/src/main.ts
+++ app/client/src/main.ts
@@ -1,86 +1,59 @@
-import { Api, createAxiosApi, RPCs } from "@previewjs/api";
-import { createController, PreviewIframeController } from "@previewjs/iframe";
+import { createAxiosApi, RPCs } from "@previewjs/api";
+import { createController } from "@previewjs/iframe";
 import {
   generateDefaultProps,
   generatePropsAssignmentSource,
 } from "@previewjs/properties";
 
 const iframe = document.getElementById("root") as HTMLIFrameElement;
-const state = new PreviewState();
+const rpcApi = createAxiosApi("/api/");
+const iframeController = createController({
+  getIframe: () => iframe,
+  listener: () => {
+    // No-op.
+  },
+});
 
-state.start().catch(console.error);
-
-class PreviewState {
-  private readonly iframeController: PreviewIframeController;
-  private readonly rpcApi: Api;
-
-  constructor() {
-    this.rpcApi = createAxiosApi("/api/");
-    this.iframeController = createController({
-      getIframe: () => iframe,
-      listener: () => {
-        // No-op.
-      },
-    });
-  }
-
-  async start() {
-    window.addEventListener("message", this.messageListener);
-    window.addEventListener("popstate", this.popStateListener);
-    this.iframeController.start();
-    await this.onUrlChanged();
-  }
-
-  stop() {
-    this.iframeController.stop();
-    window.removeEventListener("message", this.messageListener);
-    window.removeEventListener("popstate", this.popStateListener);
-  }
-
-  private messageListener = (event: MessageEvent) => {
-    const data = event.data;
-    if (data && data.kind === "navigate") {
-      history.pushState(
-        null,
-        "",
-        `/?p=${encodeURIComponent(data.componentId)}`
-      );
-      this.onUrlChanged().catch(console.error);
-    }
-  };
-
-  private popStateListener = () => {
-    this.onUrlChanged().catch(console.error);
-  };
-
-  private async onUrlChanged() {
-    const urlParams = new URLSearchParams(document.location.search);
-    const componentId = urlParams.get("p") || "";
-    if (!componentId.includes(":")) {
-      return;
-    }
-    const [filePath, componentName] = componentId.split(":") as [
-      string,
-      string
-    ];
-    this.iframeController.resetIframe();
-    const computePropsResponse = await this.rpcApi.request(RPCs.ComputeProps, {
-      filePath,
-      componentName,
-    });
-    const defaultProps = generateDefaultProps(
-      computePropsResponse.types.props,
-      computePropsResponse.types.all
+window.addEventListener("message", (event: MessageEvent) => {
+  if (event.data && event.data.kind === "navigate") {
+    history.pushState(
+      null,
+      "",
+      `/?p=${encodeURIComponent(event.data.componentId)}`
     );
-    this.iframeController.loadComponent({
-      filePath,
-      componentName,
-      propsAssignmentSource: generatePropsAssignmentSource(
-        computePropsResponse.types.props,
-        defaultProps.keys,
-        computePropsResponse.types.all
-      ),
-      defaultPropsSource: defaultProps.source,
-    });
+    onUrlChanged().catch(console.error);
   }
+});
+window.addEventListener("popstate", () => {
+  onUrlChanged().catch(console.error);
+});
+iframeController.start();
+onUrlChanged().catch(console.error);
+
+async function onUrlChanged() {
+  const urlParams = new URLSearchParams(document.location.search);
+  const componentId = urlParams.get("p") || "";
+  if (!componentId.includes(":")) {
+    return;
+  }
+  const [filePath, componentName] = componentId.split(":") as [string, string];
+  iframeController.resetIframe();
+  const computePropsResponse = await rpcApi.request(RPCs.ComputeProps, {
+    filePath,
+    componentName,
+  });
+  const defaultProps = generateDefaultProps(
+    computePropsResponse.types.props,
+    computePropsResponse.types.all
+  );
+  iframeController.loadComponent({
+    filePath,
+    componentName,
+    propsAssignmentSource: generatePropsAssignmentSource(
+      computePropsResponse.types.props,
+      defaultProps.keys,
+      computePropsResponse.types.all
+    ),
+    defaultPropsSource: defaultProps.source,
+  });
 }
