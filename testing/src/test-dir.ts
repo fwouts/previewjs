5c5d5c276da0a0cf1d9d62137c2bc948ec85a562
François Wouts <f@zenc.io>
2022-11-25 10:56:15 +1100

    Faster

--- testing/src/test-dir.ts
+++ testing/src/test-dir.ts
@@ -2,16 +2,21 @@ import fs from "fs-extra";
 import path from "path";
 import { sync } from "./sync";
 
-export async function prepareTestDir(testDir: string) {
+export function prepareTestDir(testDir: string) {
+  const rootDirPath = path.join(
+    testDir,
+    "..",
+    "_tmp_",
+    Buffer.from(`${path.basename(testDir)}-${process.pid}`, "utf8").toString(
+      "base64"
+    )
+  );
+  fs.mkdirpSync(rootDirPath);
   // Ensure we don't have a cache directory.
   const cacheDirPath = path.join(testDir, "node_modules", ".previewjs");
-  if (await fs.pathExists(cacheDirPath)) {
-    await fs.remove(cacheDirPath);
+  if (fs.pathExistsSync(cacheDirPath)) {
+    fs.removeSync(cacheDirPath);
   }
-  const tempParentDirPath = path.join(testDir, "..", "_tmp_");
-  await fs.mkdirp(tempParentDirPath);
-  const rootDirPath = await fs.mkdtemp(path.join(tempParentDirPath, "app-"));
-  await fs.mkdirp(rootDirPath);
-  await sync(testDir, rootDirPath);
+  sync(testDir, rootDirPath);
   return rootDirPath;
 }
