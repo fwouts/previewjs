f388ac1daba8a210592315aa4f64d1c79bd87a47
François Wouts <f@zenc.io>
2022-11-24 21:50:15 +1100

    Update smoke-tests.spec.ts

--- frameworks/svelte/tests/smoke-tests.spec.ts
+++ frameworks/svelte/tests/smoke-tests.spec.ts
@@ -11,8 +11,14 @@ test.describe("smoke tests", () => {
   for (const [appName, componentId] of [
     ["svelte", "src/App.svelte:App"],
     ["svelte-app", "src/App.svelte:App"],
-    ["sveltekit-app", "src/routes/+page.svelte:+page"],
-    ["sveltekit-demo", "src/routes/Header.svelte:Header"],
+    ...(parseInt(process.versions.node.split(".")[0]!) >= 16
+      ? [
+          // SvelteKit requires Node 16.
+          // See https://github.com/sveltejs/kit/issues/2412
+          ["sveltekit-app", "src/routes/+page.svelte:+page"],
+          ["sveltekit-demo", "src/routes/Header.svelte:Header"],
+        ]
+      : []),
   ] as const) {
     const appDir = smokeTestApp(appName);
     previewTest([pluginFactory], appDir)(appName, async (preview) => {
