b542e81681cc61ef49e448caff779bf6aa917746
François Wouts <f@zenc.io>
2022-11-25 10:25:57 +1100

    Update custom-preview.spec.ts

--- frameworks/solid/tests/custom-preview.spec.ts
+++ frameworks/solid/tests/custom-preview.spec.ts
@@ -6,7 +6,6 @@ import pluginFactory from "../src";
 test.describe.configure({ mode: "parallel" });
 
 const originalSource = `
-import React from "react";
 export function Button(props: { label: string; disabled?: boolean }) {
   return (
     <button id="button" disabled={props.disabled}>
@@ -16,89 +15,84 @@ export function Button(props: { label: string; disabled?: boolean }) {
 }
 `;
 
-const testApp = (suffix: string | number) =>
-  path.join(__dirname, "apps", "react" + suffix);
+const testApp = path.join(__dirname, "apps", "solid");
 
-for (const reactVersion of [16, 17, 18]) {
-  test.describe(`v${reactVersion}`, () => {
-    test.describe("react/custom preview", () => {
-      const test = previewTest([pluginFactory], testApp(reactVersion));
+test.describe("solid/custom preview", () => {
+  const test = previewTest([pluginFactory], testApp);
 
-      test("shows variants when already configured", async (preview) => {
-        await preview.fileManager.update(
-          "src/Button.tsx",
-          `import { setupPreviews } from '@previewjs/plugin-react/setup';
-          ${originalSource}
-          setupPreviews(Button, {
-            default: {
-              label: "default variant",
-            },
-            disabled: {
-              label: "disabled variant",
-              disabled: true,
-            },
-          });`
-        );
-        await preview.show("src/Button.tsx:Button", { variantKey: "default" });
-        await preview.iframe.waitForSelector(
-          "xpath=//button[contains(., 'default variant')]"
-        );
-      });
-
-      test("supports variants defined as function", async (preview) => {
-        await preview.fileManager.update(
-          "src/Button.tsx",
-          `import { setupPreviews } from '@previewjs/plugin-react/setup';
-          ${originalSource}
-          setupPreviews(Button, () => ({
-            default: {
-              label: "custom label",
-            },
-          }));`
-        );
-        await preview.show("src/Button.tsx:Button", { variantKey: "default" });
-        await preview.iframe.waitForSelector(
-          "xpath=//button[contains(., 'custom label')]"
-        );
-      });
-
-      test("updates when preview is updated", async (preview) => {
-        await preview.fileManager.update(
-          "src/Button.tsx",
-          `import { setupPreviews } from '@previewjs/plugin-react/setup';
-          ${originalSource}
-          setupPreviews(Button, {
-            default: {
-              label: "default",
-            },
-            disabled: {
-              label: "disabled",
-              disabled: true,
-            },
-          });`
-        );
-        await preview.show("src/Button.tsx:Button", { variantKey: "default" });
-        await preview.iframe.waitForSelector(
-          "xpath=//button[contains(., 'default')]"
-        );
-        await preview.fileManager.update(
-          "src/Button.tsx",
-          `import { setupPreviews } from '@previewjs/plugin-react/setup';
-          ${originalSource}
-          setupPreviews(Button, {
-            default: {
-              label: "foo label",
-            },
-            disabled: {
-              label: "disabled",
-              disabled: true,
-            },
-          });`
-        );
-        await preview.iframe.waitForSelector(
-          "xpath=//button[contains(., 'foo label')]"
-        );
-      });
-    });
+  test("shows variants when already configured", async (preview) => {
+    await preview.fileManager.update(
+      "src/Button.tsx",
+      `import { setupPreviews } from '@previewjs/plugin-solid/setup';
+      ${originalSource}
+      setupPreviews(Button, {
+        default: {
+          label: "default variant",
+        },
+        disabled: {
+          label: "disabled variant",
+          disabled: true,
+        },
+      });`
+    );
+    await preview.show("src/Button.tsx:Button", { variantKey: "default" });
+    await preview.iframe.waitForSelector(
+      "xpath=//button[contains(., 'default variant')]"
+    );
   });
-}
+
+  test("supports variants defined as function", async (preview) => {
+    await preview.fileManager.update(
+      "src/Button.tsx",
+      `import { setupPreviews } from '@previewjs/plugin-solid/setup';
+      ${originalSource}
+      setupPreviews(Button, () => ({
+        default: {
+          label: "custom label",
+        },
+      }));`
+    );
+    await preview.show("src/Button.tsx:Button", { variantKey: "default" });
+    await preview.iframe.waitForSelector(
+      "xpath=//button[contains(., 'custom label')]"
+    );
+  });
+
+  test("updates when preview is updated", async (preview) => {
+    await preview.fileManager.update(
+      "src/Button.tsx",
+      `import { setupPreviews } from '@previewjs/plugin-solid/setup';
+      ${originalSource}
+      setupPreviews(Button, {
+        default: {
+          label: "default",
+        },
+        disabled: {
+          label: "disabled",
+          disabled: true,
+        },
+      });`
+    );
+    await preview.show("src/Button.tsx:Button", { variantKey: "default" });
+    await preview.iframe.waitForSelector(
+      "xpath=//button[contains(., 'default')]"
+    );
+    await preview.fileManager.update(
+      "src/Button.tsx",
+      `import { setupPreviews } from '@previewjs/plugin-solid/setup';
+      ${originalSource}
+      setupPreviews(Button, {
+        default: {
+          label: "foo label",
+        },
+        disabled: {
+          label: "disabled",
+          disabled: true,
+        },
+      });`
+    );
+    await preview.iframe.waitForSelector(
+      "xpath=//button[contains(., 'foo label')]"
+    );
+  });
+});
