7b2eeeac8331acf1b2f7a0337b9778f260131f25
François Wouts <f@zenc.io>
2022-11-24 15:26:15 +1100

    Add tests

---
+++ frameworks/vue2/tests/jsx.spec.ts
@@ -0,0 +1,51 @@
+import test from "@playwright/test";
+import { previewTest } from "@previewjs/testing";
+import path from "path";
+import pluginFactory from "../src";
+
+test.describe.configure({ mode: "parallel" });
+
+const testApp = path.join(__dirname, "apps", "vue2");
+
+test.describe("vue2/jsx", () => {
+  const test = previewTest([pluginFactory], testApp);
+
+  test("renders JSX component", async (preview) => {
+    await preview.fileManager.update(
+      "src/Button.jsx",
+      `export const Button = ({props}) => {
+        return <button>
+        {props.label || "a button"}
+        </button>
+      }`
+    );
+    await preview.show("src/Button.jsx:Button");
+    await preview.iframe.waitForSelector(
+      "xpath=//button[contains(., 'a button')]"
+    );
+  });
+
+  test("renders JSX component with previews", async (preview) => {
+    await preview.fileManager.update(
+      "src/Button.jsx",
+      `export const Button = ({props}) => {
+        return <button>
+          {props.label || "a button"}
+        </button>
+      }
+
+      Button.previews = {
+        default: {
+          label: "Hello, World!"
+        }
+      }`
+    );
+    await preview.show(
+      "src/Button.jsx:Button",
+      `properties = variants[0].props`
+    );
+    await preview.iframe.waitForSelector(
+      "xpath=//button[contains(., 'Hello, World!')]"
+    );
+  });
+});
