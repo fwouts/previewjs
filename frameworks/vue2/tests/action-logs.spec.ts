c707f08669fe4a9d06533f39c8be82a9829871eb
François Wouts <f@zenc.io>
2022-11-24 13:16:30 +1100

    Vue 2

---
+++ frameworks/vue2/tests/action-logs.spec.ts
@@ -0,0 +1,34 @@
+import test, { expect } from "@playwright/test";
+import { previewTest } from "@previewjs/testing";
+import path from "path";
+import pluginFactory from "../src";
+
+test.describe.configure({ mode: "parallel" });
+
+const testApp = path.join(__dirname, "apps", "vue2");
+
+test.describe("vue2/action logs", () => {
+  const test = previewTest([pluginFactory], testApp);
+
+  test("shows action logs on link click", async (preview) => {
+    await preview.fileManager.update(
+      "src/App.vue",
+      `<template>
+        <a id="link" href="https://www.google.com">
+          Hello, World!
+        </a>
+      </template>`
+    );
+    await preview.show("src/App.vue:App");
+    const link = await preview.iframe.waitForSelector("#link");
+    preview.events.clear();
+    await link.click();
+    expect(preview.events.get()).toEqual([
+      {
+        kind: "action",
+        path: "https://www.google.com/",
+        type: "url",
+      },
+    ]);
+  });
+});
