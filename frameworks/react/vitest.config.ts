643de1c3d7f497666bbdd097329b2fdcd0520d2d
François Wouts <f@zenc.io>
2022-11-28 09:40:38 +1100

    chore: always exclude "preview" directory from tests

--- frameworks/react/vitest.config.ts
+++ frameworks/react/vitest.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    exclude: ["node_modules", "dist", "tests"],
+    exclude: ["node_modules", "dist", "preview", "tests"],
   },
 });
