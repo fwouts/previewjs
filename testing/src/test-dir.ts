248df62aa0606961ca31661a3e192379a3a495c2
François Wouts <f@zenc.io>
2022-11-28 16:47:06 +1100

    chore: move sync() to its only usage file

--- testing/src/test-dir.ts
+++ testing/src/test-dir.ts
@@ -1,6 +1,16 @@
-import fs from "fs-extra";
+import {
+  copyFileSync,
+  lstatSync,
+  mkdirpSync,
+  mkdirSync,
+  pathExistsSync,
+  readdirSync,
+  removeSync,
+  statSync,
+  symlinkSync,
+  unlinkSync,
+} from "fs-extra";
 import path from "path";
-import { sync } from "./sync";
 
 export function prepareTestDir(testDir: string) {
   const rootDirPath = path.join(
@@ -9,7 +19,73 @@ export function prepareTestDir(testDir: string) {
     "_tmp_",
     `${path.basename(testDir)}-${process.pid}`
   );
-  fs.mkdirpSync(rootDirPath);
+  mkdirpSync(rootDirPath);
   sync(testDir, rootDirPath);
   return rootDirPath;
+}
+
+function sync(srcPath: string, dstPath: string): void {
+  if (!pathExistsSync(srcPath)) {
+    throw new Error(`No such directory: ${srcPath}`);
+  }
+  if (!pathExistsSync(dstPath)) {
+    mkdirSync(dstPath, { recursive: true });
+  }
+
+  // Keep track of existing files so we can remove the old ones later.
+  const existingFiles = new Set(readdirSync(dstPath));
+
+  // Update all source files.
+  const dirStat = pathExistsSync(srcPath) && statSync(srcPath);
+  if (!dirStat || !dirStat.isDirectory()) {
+    throw new Error(`Expected a directory at ${srcPath}`);
+  }
+
+  for (const name of readdirSync(srcPath)) {
+    existingFiles.delete(name);
+    const sourceFilePath = path.join(srcPath, name);
+    const destinationFilePath = path.join(dstPath, name);
+    const fileStat = lstatSync(sourceFilePath);
+    if (name === "node_modules") {
+      if (pathExistsSync(destinationFilePath)) {
+        continue;
+      }
+      mkdirSync(destinationFilePath);
+      for (const f of readdirSync(sourceFilePath)) {
+        if (f === ".previewjs") {
+          // Ignore.
+          continue;
+        }
+        symlinkSync(
+          path.join(sourceFilePath, f),
+          path.join(destinationFilePath, f)
+        );
+      }
+    } else if (fileStat.isSymbolicLink()) {
+      // Ignore it.
+    } else if (fileStat.isFile()) {
+      if (pathExistsSync(destinationFilePath)) {
+        const destinationFileStat = statSync(destinationFilePath);
+        if (destinationFileStat.isDirectory()) {
+          removeSync(destinationFilePath);
+        } else if (!destinationFileStat.isFile()) {
+          unlinkSync(destinationFilePath);
+        }
+      }
+      copyFileSync(sourceFilePath, destinationFilePath);
+    } else {
+      sync(sourceFilePath, destinationFilePath);
+    }
+  }
+
+  // Remove any old files.
+  for (const f of existingFiles) {
+    const absoluteFilePath = path.join(dstPath, f);
+    const fileStat = statSync(absoluteFilePath);
+    if (fileStat.isDirectory()) {
+      removeSync(absoluteFilePath);
+    } else {
+      unlinkSync(absoluteFilePath);
+    }
+  }
 }
