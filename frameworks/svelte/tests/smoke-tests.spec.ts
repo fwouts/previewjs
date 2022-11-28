7b82f1f3d1817d0441184b79d86ab120657041c4
François Wouts <f@zenc.io>
2022-11-28 14:31:08 +1100

    test: wait for #ready in smoke tests (#1250)

--- frameworks/svelte/tests/smoke-tests.spec.ts
+++ frameworks/svelte/tests/smoke-tests.spec.ts
@@ -7,7 +7,6 @@ const smokeTestApp = (name: string) => path.join(__dirname, "apps", name);
 
 test.describe("smoke tests", () => {
   for (const [appName, componentId] of [
-    ["svelte", "src/App.svelte:App"],
     ["svelte-app", "src/App.svelte:App"],
     ...(parseInt(process.versions.node.split(".")[0]!) >= 16
       ? [
@@ -21,6 +20,7 @@ test.describe("smoke tests", () => {
     const appDir = smokeTestApp(appName);
     previewTest([pluginFactory], appDir)(appName, async (preview) => {
       await preview.show(componentId);
+      await preview.iframe.waitForSelector("#ready");
       await preview.iframe.takeScreenshot(
         path.join(appDir, `__screenshot__${process.platform}.png`)
       );
--- frameworks/vue2/tests/smoke-tests.spec.ts
+++ frameworks/vue2/tests/smoke-tests.spec.ts
@@ -8,12 +8,12 @@ const smokeTestApp = (name: string) => path.join(__dirname, "apps", name);
 test.describe("smoke tests", () => {
   for (const [appName, componentId] of [
     ["nuxt2-app", "pages/index.vue:index"],
-    ["vue2", "src/App.vue:App"],
     ["vue2-app", "src/App.vue:App"],
   ] as const) {
     const appDir = smokeTestApp(appName);
     previewTest([pluginFactory], appDir)(appName, async (preview) => {
       await preview.show(componentId);
+      await preview.iframe.waitForSelector("#ready");
       await preview.iframe.takeScreenshot(
         path.join(appDir, `__screenshot__${process.platform}.png`)
       );
