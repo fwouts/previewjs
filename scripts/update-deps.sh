73f134192ba6905cdde7a2d5421034879ceb1666
François Wouts <f@zenc.io>
2022-12-23 22:09:50 +1100

    revert vite-tsconfig-paths

--- scripts/update-deps.sh
+++ scripts/update-deps.sh
@@ -2,7 +2,7 @@ set -e
 
 # Update all dependencies to latest except frameworks (and test apps).
 # Exclude problematic packages for now.
-pnpm --filter "!./frameworks/**/*" -r update !env-paths !get-port !globby !execa --latest
+pnpm --filter "!./frameworks/**/*" -r update !env-paths !get-port !globby !execa !vite-tsconfig-paths --latest
 
 # Update all other dependencies within their range.
 pnpm -r update
