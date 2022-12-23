5abc68fbeb0f32eac4059f92575adb7768e35db8
François Wouts <f@zenc.io>
2022-12-23 22:34:18 +1100

    chore: update dependencies (#1310)

---
+++ scripts/update-deps.sh
@@ -0,0 +1,11 @@
+set -e
+
+# Update all dependencies to latest except frameworks (and test apps).
+# Exclude problematic packages for now.
+pnpm --filter "!./frameworks/**/*" -r update !env-paths !get-port !globby !execa !vite-tsconfig-paths --latest
+
+# Update all other dependencies within their range.
+pnpm -r update
+
+# Ensure all packages are installed correctly.
+pnpm i
