2b5a34a67d64a9526af6cd7d2c5b5f1ac285474b
François Wouts <f@zenc.io>
2023-01-04 23:54:08 +1100

    extract smoke test logic

---
+++ testing/src/smoke-test.ts
@@ -0,0 +1,42 @@
+import type { FrameworkPluginFactory } from "@previewjs/core";
+import path from "path";
+import { previewTest } from "./preview-test";
+
+export function smokeTests({
+  projectsDir,
+  pluginFactory,
+  componentIdsPerProject,
+}: {
+  projectsDir: string;
+  pluginFactory: FrameworkPluginFactory;
+  componentIdsPerProject: Record<string, string[]>;
+}) {
+  for (const [projectName, componentIds] of Object.entries(
+    componentIdsPerProject
+  )) {
+    const appDir = path.join(projectsDir, projectName);
+    for (const componentId of componentIds) {
+      const [filePath, componentName] = componentId.split(":") as [
+        string,
+        string
+      ];
+      previewTest([pluginFactory], appDir)(
+        `${projectName}/${componentId}`,
+        async (preview) => {
+          await preview.show(componentId);
+          await preview.iframe.waitForSelector("#ready");
+          const fileExt = path.extname(filePath);
+          await preview.iframe.takeScreenshot(
+            path.join(
+              appDir,
+              `${filePath.substring(
+                0,
+                filePath.length - fileExt.length
+              )}_${componentName}_screenshot_${process.platform}.png`
+            )
+          );
+        }
+      );
+    }
+  }
+}
