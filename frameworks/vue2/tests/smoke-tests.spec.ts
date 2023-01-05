52ec6d5aa59f70c886fc8559d3fef9b6f4b77b71
François Wouts <f@zenc.io>
2023-01-05 22:59:38 +1100

    Update smoke-tests.spec.ts

--- frameworks/vue2/tests/smoke-tests.spec.ts
+++ frameworks/vue2/tests/smoke-tests.spec.ts
@@ -10,10 +10,7 @@ test.describe.parallel("smoke tests", () => {
     componentIdsPerProject: {
       "nuxt2-app": ["pages/index.vue:index"],
       "vue2-app": ["src/App.vue:App"],
-      "vue2-storybook": [
-        "src/stories/Button.stories.js:Primary",
-        "src/stories/Page.stories.js:LoggedIn",
-      ],
+      "vue2-storybook": ["src/stories/Button.stories.js:Primary"],
     },
   });
 });
