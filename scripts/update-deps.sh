a79b13589ba9c5846b7a32a8abed1409f05255a1
François Wouts <f@zenc.io>
2023-01-17 22:43:28 +1100

    chore: prevent auto-update of @types/vscode

--- scripts/update-deps.sh
+++ scripts/update-deps.sh
@@ -2,7 +2,7 @@ set -e
 
 # Update all dependencies to latest except frameworks (and test apps).
 # Exclude problematic packages for now.
-pnpm --filter "!./frameworks/**/*" -r update !env-paths !get-port !globby !execa !inquirer !@types/inquirer !vite-tsconfig-paths --latest
+pnpm --filter "!./frameworks/**/*" -r update !env-paths !get-port !globby !execa !inquirer !@types/inquirer !@types/vscode !vite-tsconfig-paths --latest
 
 # Update all other dependencies within their range.
 pnpm -r update
