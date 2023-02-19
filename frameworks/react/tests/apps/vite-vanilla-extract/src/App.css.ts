bd27dae224f9fe4b36780feb85f02f85f7a1f7ca
François Wouts <f@zenc.io>
2023-02-19 22:30:58 +1100

    chore: update dependencies (#1377)

--- frameworks/react/tests/apps/vite-vanilla-extract/src/App.css.ts
+++ frameworks/react/tests/apps/vite-vanilla-extract/src/App.css.ts
@@ -1,21 +1,24 @@
 import { keyframes, style } from "@vanilla-extract/css";
 
-export const appStyle = style({
+// TODO: Use "const" instead of "var".
+// As of Feb 2023, this seems to trigger an error with esbuild trying to target es5 for some reason.
+
+export var appStyle = style({
   textAlign: "center",
 });
 
-const appLogoSpin = keyframes({
+var appLogoSpin = keyframes({
   from: { transform: "rotate(0deg)" },
   to: { transform: "rotate(360deg)" },
 });
 
-export const appLogoStyle = style({
+export var appLogoStyle = style({
   height: "40vmin",
   pointerEvents: "none",
   animation: `${appLogoSpin} infinite 20s linear`,
 });
 
-export const appHeaderStyle = style({
+export var appHeaderStyle = style({
   backgroundColor: "#282c34",
   minHeight: "100vh",
   display: "flex",
@@ -26,6 +29,6 @@ export const appHeaderStyle = style({
   color: "white",
 });
 
-export const appLinkStyle = style({
+export var appLinkStyle = style({
   color: "#61dafb",
 });
--- scripts/update-deps.sh
+++ scripts/update-deps.sh
@@ -2,10 +2,13 @@ set -e
 
 # Update all dependencies to latest except frameworks (and test apps).
 # Exclude problematic packages for now.
-pnpm --filter "!./frameworks/**/*" -r update !env-paths !get-port !globby !execa !inquirer !@types/inquirer !@types/vscode !vite-tsconfig-paths --latest
+pnpm --filter "!./dev-workspace" --filter "!./frameworks/**/*" -r update !env-paths !get-port !globby !execa !inquirer !@types/inquirer !@types/vscode !vite-tsconfig-paths !turbo --latest
 
 # Update all other dependencies within their range.
-pnpm -r update
+pnpm --filter "!./dev-workspace" -r update
+
+# Revert changes from "major.minor" to "major.minor".
+LC_ALL=C && LANG=C && find . -type f | grep -v node_modules | grep -v .run | xargs sed -i '' 's/"/"/g'
 
 # Ensure all packages are installed correctly.
 pnpm i
