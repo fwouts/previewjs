0fba3016b0317eeb0b0d9842285f0d62c7c57928
François Wouts <f@zenc.io>
2023-02-24 11:11:01 +1100

    fix(intellij): explicitly specify supported IDE versions as 2023.1 may be incompatible

--- integrations/intellij/build.gradle.kts
+++ integrations/intellij/build.gradle.kts
@@ -89,6 +89,7 @@ tasks {
     patchPluginXml {
         version.set(properties("pluginVersion"))
         sinceBuild.set(properties("pluginSinceBuild"))
+        untilBuild.set(properties("pluginUntilBuild"))
     }
 
     runPluginVerifier {
