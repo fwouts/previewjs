6fe475e9fd512de92072dd29515bc404c247d6a5
François Wouts <f@zenc.io>
2023-03-03 16:01:34 +1100

    chore: exclude @previewjs/* from update script
    This will prevent it from messing up and replacing "workspace:*" with exact versions in devDependencies.

--- scripts/update-deps.sh
+++ scripts/update-deps.sh
@@ -2,10 +2,10 @@ set -e
 
 # Update all dependencies to latest except frameworks (and test apps).
 # Exclude problematic packages for now.
-pnpm --filter "!./dev-workspace" --filter "!./frameworks/**/*" -r update !env-paths !get-port !globby !execa !inquirer !@types/inquirer !@types/vscode !vite-tsconfig-paths !turbo --latest
+pnpm --filter "!./dev-workspace" --filter "!./frameworks/**/*" -r update !env-paths !get-port !globby !execa !inquirer !@types/inquirer !@types/vscode !vite-tsconfig-paths "!@previewjs/*" !turbo --latest
 
 # Update all other dependencies within their range.
-pnpm --filter "!./dev-workspace" -r update !@types/vscode
+pnpm --filter "!./dev-workspace" -r update !@types/vscode "!@previewjs/*"
 
 # Revert changes from "major.minor" to "major.minor".
 LC_ALL=C && LANG=C && find . -type f | grep -v node_modules | grep -v .run | xargs sed -i '' 's/"/"/g'
