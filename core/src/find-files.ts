c9e97f0d3f16b2c506eeb07386a0844bfd25df88
François Wouts <f@zenc.io>
2023-03-06 22:31:13 +1100

    chore: update globby

--- core/src/find-files.ts
+++ core/src/find-files.ts
@@ -1,5 +1,5 @@
 import fs from "fs-extra";
-import globby from "globby";
+import { globby } from "globby";
 import path from "path";
 
 export async function findFiles(rootDirPath: string, pattern: string) {
