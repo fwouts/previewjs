562f21b365d5a60bb671e9da3785a2099431706b
François Wouts <f@zenc.io>
2023-03-16 23:22:58 +1100

    Update local-eval.ts

--- core/src/vite/plugins/local-eval.ts
+++ core/src/vite/plugins/local-eval.ts
@@ -3,9 +3,20 @@ import type * as vite from "vite";
 export function localEval(): vite.Plugin {
   return {
     name: "previewjs:local-eval",
-    transform: (code) => {
+    enforce: "post",
+    transform: (code, id) => {
+      if (id.indexOf(`/node_modules/`) !== -1) {
+        return null;
+      }
+      if (id.indexOf("?") !== -1) {
+        // Example: .../src/App.vue?vue&type=style&index=0&lang.css
+        return null;
+      }
       return (
         code +
+        // Note: This is an uppercase function so that it "looks like" a React component.
+        // This needed to prevent breaking React Refresh, which expects all exports to
+        // be React components.
         `
 export const PreviewJsEvaluateLocally = async (autogenCallbackPropsSource, propsAssignmentSource) => {
   let autogenCallbackProps = {};
