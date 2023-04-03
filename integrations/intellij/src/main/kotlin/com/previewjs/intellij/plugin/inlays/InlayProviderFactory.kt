d2bad9fe001140333a2ba0133f7e60f575e628b3
François Wouts <f@zenc.io>
2023-04-03 20:40:09 +1000

    Fix Vue support

--- integrations/intellij/src/main/kotlin/com/previewjs/intellij/plugin/inlays/InlayProviderFactory.kt
+++ integrations/intellij/src/main/kotlin/com/previewjs/intellij/plugin/inlays/InlayProviderFactory.kt
@@ -22,9 +22,10 @@ import javax.swing.JPanel
 @Suppress("UnstableApiUsage")
 class InlayProviderFactory : InlayHintsProviderFactory {
     companion object {
-        // Note: "textmate" is what's used in IntelliJ CE for JS/TS(X) files.
+        // Note #1: "textmate" is what's used in IntelliJ CE for JS/TS(X) files.
+        // Note #2: "TEXT" is what's used in IntelliJ CE for Vue files.
         val LANGUAGE_IDS =
-            setOf("textmate", "ECMAScript 6", "JavaScript", "TypeScript", "TypeScript JSX", "SvelteHTML", "VueJS")
+            setOf("TEXT", "textmate", "ECMAScript 6", "JavaScript", "TypeScript", "TypeScript JSX", "SvelteHTML", "VueJS")
 
         // Full list from IntelliJ IDEA 2023.1:
         // TOML, JSON, HgIgnore, InjectedFreeMarker, MySQL, AZURE, TypeScript, AIDL, AngularJS, PostCSS, Snowflake, Micronaut-MongoDB-JSON, Redis, XML, SQL92, TSQL, protobase, Angular2Svg, JSUnicodeRegexp, Nashorn JS, JVM, EL, Gherkin, AndroidDataBinding, SQLDateTime, SVG, , XHTML, RoomSql, DB2, Properties, XPath, DB2_ZOS, FTL>, JavaScript 1.8, ThymeleafSpringSecurityExtras, Renderscript, Angular2, prototext, ThymeleafTemplatesExpressions, H2, XsdRegExp, HTML, LESS, JQL, yaml, MongoJSExt, JSPX, Flow JS, PostgreSQL, JQuery-CSS, GitIgnore, Lombok.Config, Dockerfile, KND, CouchbaseQuery, Qute, JSRegexp, ThymeleafExpressions, VueExpr, SQLite, SparkSQL, GenericSQL, JSP, OracleSqlPlus, UastContextLanguage, Markdown, DTD, TEXT, DeviceSpec, UAST, ThymeleafUrlExpressions, EQL, Groovy, TypeScript JSX, SCSS, JSONPath, JSON5, Vue, Exasol, HSQLDB, protobuf, EditorConfig, ECMA Script Level 4, Greenplum, Cookie, kotlin, textmate, ClickHouse, HtmlCompatible, EJBQL, Derby, SPI, Cockroach, JavaScript, Angular2Html, MicronautDataQL, IntegrationPerformanceTest, VTL, GitExclude, MultiDexKeep, Shell Script, CassandraQL, RegExp, HiveQL, Smali, Manifest, SHRINKER_CONFIG, JAVA, LogcatFilter, VueJS, IgnoreLang, SQL, $XSLT, PointcutExpression, MariaDB, DB2_IS, AGSL, Oracle, SpEL, SpringDataQL, JSON Lines, FTL], BigQuery, MongoJS, YouTrack, CSS, MongoDB, Metadata JSON, Vertica, SASS, Sybase, ThymeleafIterateExpressions, ThymeleafTemplatesFragmentExpressions, ECMAScript 6, XPath2, HTTP Request, RELAX-NG, DockerIgnore, HttpClientHandlerJavaScriptDialect, FTL, JPAQL, HQL, JShellLanguage, VueTS, MySQL based, MongoDB-JSON, Spring-MongoDB-JSON, Redshift
