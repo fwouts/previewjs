efda8c089134777769703fc7d9cd2fdbc42869cf
François Wouts <f@zenc.io>
2023-04-19 23:23:19 +1000

    fix(vue3): fully support CSF3 TypeScript stories

---
+++ framework-plugins/vue3/tests/apps/vite-storybook-ts/src/stories/Header.stories.ts
@@ -0,0 +1,42 @@
+import type { Meta, StoryObj } from '@storybook/vue3';
+
+import MyHeader from './Header.vue';
+
+const meta = {
+  /* 👇 The title prop is optional.
+   * See https://storybook.js.org/docs/vue/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
+  title: 'Example/Header',
+  component: MyHeader,
+  render: (args: any) => ({
+    components: { MyHeader },
+    setup() {
+      return { args };
+    },
+    template: '<my-header :user="args.user" />',
+  }),
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
+    layout: 'fullscreen',
+  },
+  // This component will have an automatically generated docsPage entry: https://storybook.js.org/docs/vue/writing-docs/autodocs
+  tags: ['autodocs'],
+} satisfies Meta<typeof MyHeader>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const LoggedIn: Story = {
+  args: {
+    user: {
+      name: 'Jane Doe',
+    },
+  },
+};
+
+export const LoggedOut: Story = {
+  args: {
+    user: null,
+  },
+};
--- storybook-helpers/src/extract-default-component.ts
+++ storybook-helpers/src/extract-default-component.ts
@@ -3,24 +3,49 @@ import ts from "typescript";
 export function extractDefaultComponent(
   sourceFile: ts.SourceFile
 ): ts.Expression | null {
+  const entities: Record<string, ts.Expression> = {};
   for (const statement of sourceFile.statements) {
-    if (ts.isExportAssignment(statement)) {
-      let exportedValue = statement.expression;
-      while (ts.isAsExpression(exportedValue)) {
-        exportedValue = exportedValue.expression;
-      }
-      if (ts.isObjectLiteralExpression(exportedValue)) {
-        for (const property of exportedValue.properties) {
-          if (
-            ts.isPropertyAssignment(property) &&
-            ts.isIdentifier(property.name) &&
-            property.name.text === "component"
-          ) {
-            return property.initializer;
-          }
+    if (ts.isVariableStatement(statement)) {
+      for (const declaration of statement.declarationList.declarations) {
+        if (ts.isIdentifier(declaration.name) && declaration.initializer) {
+          entities[declaration.name.text] = declaration.initializer;
         }
       }
+    } else if (ts.isExportAssignment(statement)) {
+      return extractComponent(statement.expression);
     }
   }
+
+  function extractComponent(expression: ts.Expression): ts.Expression | null {
+    expression = unwrapExpression(expression);
+    if (ts.isObjectLiteralExpression(expression)) {
+      for (const property of expression.properties) {
+        if (
+          ts.isPropertyAssignment(property) &&
+          ts.isIdentifier(property.name) &&
+          property.name.text === "component"
+        ) {
+          return property.initializer;
+        }
+      }
+    } else if (ts.isIdentifier(expression)) {
+      const value = entities[expression.text];
+      if (value) {
+        return extractComponent(value);
+      }
+    }
+    return null;
+  }
+
   return null;
+}
+
+function unwrapExpression(expression: ts.Expression): ts.Expression {
+  while (
+    ts.isAsExpression(expression) ||
+    ts.isSatisfiesExpression(expression)
+  ) {
+    expression = expression.expression;
+  }
+  return expression;
 }
