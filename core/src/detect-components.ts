f764dda73ea1cef8df196dbc62f6a0dfb8fb8107
François Wouts <f@zenc.io>
2023-04-21 16:36:55 +1000

    chore: remove automatic sorting of components
    There is value in preserving the code-defined order of components and stories.

--- core/src/detect-components.ts
+++ core/src/detect-components.ts
@@ -104,9 +104,6 @@ export function detectComponents(
       changedAbsoluteFilePaths
     );
     const components = [...recycledComponents, ...refreshedComponents];
-    components.sort((a, b) => {
-      return a.componentId.localeCompare(b.componentId);
-    });
     if (!options.filePaths) {
       await fs.mkdirp(path.dirname(cacheFilePath));
       const updatedCache: CachedProjectComponents = {
