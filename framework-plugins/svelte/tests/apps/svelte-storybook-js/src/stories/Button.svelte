026308e77bd8ce440ed11c9c0763321b4ab113ab
François Wouts <f@zenc.io>
2023-04-26 12:23:09 +1000

    Add test apps

---
+++ framework-plugins/svelte/tests/apps/svelte-storybook-js/src/stories/Button.svelte
@@ -0,0 +1,36 @@
+<script>
+  import './button.css';
+
+  /**
+   * Is this the principal call to action on the page?
+   */
+  export let primary = false;
+
+  /**
+   * @type {string} What background color to use
+   */
+  export let backgroundColor = undefined;
+
+  /**
+   * @type {'small' | 'medium' | 'large'} How large should the button be?
+   */
+  export let size = 'medium';
+
+  /**
+   * @type {string} Button contents
+   */
+  export let label;
+
+  $: mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+
+  $: style = backgroundColor ? `background-color: ${backgroundColor}` : '';
+</script>
+
+<button
+  type="button"
+  class={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+  {style}
+  on:click
+>
+  {label}
+</button>
---
+++ framework-plugins/svelte/tests/apps/svelte-storybook-ts/.storybook/main.ts
@@ -0,0 +1,17 @@
+import type { StorybookConfig } from "@storybook/svelte-vite";
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx|svelte)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/svelte-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
+export default config;
