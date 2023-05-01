b21deb137e92b6431a1308fd256ab76f5c2569a7
François Wouts <f@zenc.io>
2023-05-01 13:07:54 +1000

    chore: remove vite-tsconfig-paths from update blocklist

--- scripts/update-deps.sh
+++ scripts/update-deps.sh
@@ -2,7 +2,7 @@ set -e
 
 # Update all dependencies to latest except framework-plugins (and test apps).
 # Exclude problematic packages for now.
-pnpm --filter "!./dev-workspace" --filter "!./framework-plugins/**/*" -r update !@types/vscode !vite-tsconfig-paths "!@previewjs/*" --latest
+pnpm --filter "!./dev-workspace" --filter "!./framework-plugins/**/*" -r update !@types/vscode "!@previewjs/*" --latest
 
 # Update all other dependencies within their range.
 pnpm --filter "!./dev-workspace" -r update !@types/vscode "!@previewjs/*"
