1bf84bda17703863977bd196daa287674c5597b2
François Wouts <f@zenc.io>
2023-05-15 23:27:46 +1000

    fix

--- framework-plugins/vue2/src/vue-reader.spec.ts
+++ framework-plugins/vue2/src/vue-reader.spec.ts
@@ -36,38 +36,42 @@ export default {
       throw new Error();
     }
     expect(await virtualFile.read()).toMatchInlineSnapshot(`
+      "
 
-export default {
-  name: 'App',
-  props: {}
-}
+      export default {
+        name: 'App',
+        props: {}
+      }
 
-const pjs_component = {
-    name: "App",
-    props: {}
-} as const;
+      const pjs_component = {
+          name: \\"App\\",
+          props: {}
+      } as const;
 
 
-import type {Prop as PJS_Prop} from 'vue/types/options';
+      import type {Prop as PJS_Prop} from 'vue/types/options';
 
-type PJS_TypeOrUnion<T> = PJS_Prop<T> | ReadonlyArray<PJS_Prop<T>>;
-type PJS_OptionalPropType<T> = PJS_TypeOrUnion<T> | {type: PJS_TypeOrUnion<T>; required?: false};
-type PJS_RequiredPropType<T> = {type: PJS_TypeOrUnion<T>; required: true};
-type PJS_OptionalPropsKeys<T> = {
-  [K in keyof T]: T[K] extends PJS_OptionalPropType<any> ? K : never;
-}[keyof T];
-type PJS_RequiredPropsKeys<T> = {
-  [K in keyof T]: T[K] extends PJS_RequiredPropType<any> ? K : never;
-}[keyof T];
-type PJS_CombinedProps<T> = T extends readonly [...any] ? {
-  [K in T[number]]: unknown
-} : ({
-  [K in PJS_OptionalPropsKeys<T>]?: T[K] extends PJS_OptionalPropType<infer S> ? S : never;
-} & {
-  [K in PJS_RequiredPropsKeys<T>]: T[K] extends PJS_RequiredPropType<infer S> ? S : never;
-});
-type PJS_ExtractProps<T> = T extends { props: any } ? PJS_CombinedProps<T['props']> : {}
-type PJS_Props = PJS_ExtractProps<typeof pjs_component>;`);
+      type PJS_TypeOrUnion<T> = PJS_Prop<T> | ReadonlyArray<PJS_Prop<T>>;
+      type PJS_OptionalPropType<T> = PJS_TypeOrUnion<T> | {type: PJS_TypeOrUnion<T>; required?: false};
+      type PJS_RequiredPropType<T> = {type: PJS_TypeOrUnion<T>; required: true};
+      type PJS_OptionalPropsKeys<T> = {
+        [K in keyof T]: T[K] extends PJS_OptionalPropType<any> ? K : never;
+      }[keyof T];
+      type PJS_RequiredPropsKeys<T> = {
+        [K in keyof T]: T[K] extends PJS_RequiredPropType<any> ? K : never;
+      }[keyof T];
+      type PJS_CombinedProps<T> = T extends readonly [...any] ? {
+        [K in T[number]]: unknown
+      } : ({
+        [K in PJS_OptionalPropsKeys<T>]?: T[K] extends PJS_OptionalPropType<infer S> ? S : never;
+      } & {
+        [K in PJS_RequiredPropsKeys<T>]: T[K] extends PJS_RequiredPropType<infer S> ? S : never;
+      });
+      type PJS_ExtractProps<T> = T extends { props: any } ? PJS_CombinedProps<T['props']> : {}
+      type PJS_Props = PJS_ExtractProps<typeof pjs_component>;
+      type PJS_Slots = [];
+      "
+    `);
   });
 
   it("ignores incompatible script lang", async () => {
