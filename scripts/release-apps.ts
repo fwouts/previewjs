e8cda3ac082475b60b446388f85a779758855b79
François Wouts <f@zenc.io>
2023-06-08 00:26:44 +1000

    chore: make loader bundle update optional

--- scripts/release-apps.ts
+++ scripts/release-apps.ts
@@ -15,82 +15,99 @@ const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 async function main() {
   await assertCleanGit();
 
-  console.log(
-    `About to update loader bundle with @previewjs/pro v${previewjsProVersion}.`
-  );
-  const {
-    default: { version: coreVersion },
-  } = await import("../core/package.json", {
-    assert: { type: "json" },
-  });
-  const {
-    default: { version: vfsVersion },
-  } = await import("../vfs/package.json", {
-    assert: { type: "json" },
-  });
-  const {
-    default: { version: preactPluginVersion },
-  } = await import("../framework-plugins/preact/package.json", {
-    assert: { type: "json" },
-  });
-  const {
-    default: { version: reactPluginVersion },
-  } = await import("../framework-plugins/react/package.json", {
-    assert: { type: "json" },
-  });
-  const {
-    default: { version: solidPluginVersion },
-  } = await import("../framework-plugins/solid/package.json", {
-    assert: { type: "json" },
-  });
-  const {
-    default: { version: sveltePluginVersion },
-  } = await import("../framework-plugins/svelte/package.json", {
-    assert: { type: "json" },
-  });
-  const {
-    default: { version: vue2PluginVersion },
-  } = await import("../framework-plugins/vue2/package.json", {
-    assert: { type: "json" },
-  });
-  const {
-    default: { version: vue3PluginVersion },
-  } = await import("../framework-plugins/vue3/package.json", {
-    assert: { type: "json" },
-  });
-  const releaseDirPath = path.join(__dirname, "..", "loader", "src", "release");
-  await fs.promises.writeFile(
-    path.join(releaseDirPath, "package.json"),
-    JSON.stringify(
-      {
-        dependencies: {
-          "@previewjs/core": coreVersion,
-          "@previewjs/plugin-preact": preactPluginVersion,
-          "@previewjs/plugin-react": reactPluginVersion,
-          "@previewjs/plugin-solid": solidPluginVersion,
-          "@previewjs/plugin-svelte": sveltePluginVersion,
-          "@previewjs/plugin-vue2": vue2PluginVersion,
-          "@previewjs/plugin-vue3": vue3PluginVersion,
-          "@previewjs/pro": previewjsProVersion,
-          "@previewjs/vfs": vfsVersion,
-        },
-      },
-      null,
-      2
-    ),
-    "utf8"
-  );
-  console.log(`Running pnpm install...`);
-  await fs.promises.unlink(path.join(releaseDirPath, "pnpm-lock.yaml"));
-  await execa("pnpm", ["install", "--lockfile-only"], {
-    cwd: releaseDirPath,
-  });
-  await execa("git", ["add", "."]);
-  if (!(await isGitClean())) {
-    await execa("git", ["commit", "-m", `release: update loader dependencies`]);
-    await execa("git", ["push", "origin", "main"]);
-  }
   const prompt = inquirer.createPromptModule();
+  const { updateLoaderPackageLock } = await prompt({
+    name: "updateLoaderPackageLock",
+    type: "confirm",
+    message: "Update loader dependencies?",
+  });
+  if (updateLoaderPackageLock) {
+    console.log(
+      `About to update loader bundle with @previewjs/pro v${previewjsProVersion}.`
+    );
+    const {
+      default: { version: coreVersion },
+    } = await import("../core/package.json", {
+      assert: { type: "json" },
+    });
+    const {
+      default: { version: vfsVersion },
+    } = await import("../vfs/package.json", {
+      assert: { type: "json" },
+    });
+    const {
+      default: { version: preactPluginVersion },
+    } = await import("../framework-plugins/preact/package.json", {
+      assert: { type: "json" },
+    });
+    const {
+      default: { version: reactPluginVersion },
+    } = await import("../framework-plugins/react/package.json", {
+      assert: { type: "json" },
+    });
+    const {
+      default: { version: solidPluginVersion },
+    } = await import("../framework-plugins/solid/package.json", {
+      assert: { type: "json" },
+    });
+    const {
+      default: { version: sveltePluginVersion },
+    } = await import("../framework-plugins/svelte/package.json", {
+      assert: { type: "json" },
+    });
+    const {
+      default: { version: vue2PluginVersion },
+    } = await import("../framework-plugins/vue2/package.json", {
+      assert: { type: "json" },
+    });
+    const {
+      default: { version: vue3PluginVersion },
+    } = await import("../framework-plugins/vue3/package.json", {
+      assert: { type: "json" },
+    });
+    const releaseDirPath = path.join(
+      __dirname,
+      "..",
+      "loader",
+      "src",
+      "release"
+    );
+    await fs.promises.writeFile(
+      path.join(releaseDirPath, "package.json"),
+      JSON.stringify(
+        {
+          dependencies: {
+            "@previewjs/core": coreVersion,
+            "@previewjs/plugin-preact": preactPluginVersion,
+            "@previewjs/plugin-react": reactPluginVersion,
+            "@previewjs/plugin-solid": solidPluginVersion,
+            "@previewjs/plugin-svelte": sveltePluginVersion,
+            "@previewjs/plugin-vue2": vue2PluginVersion,
+            "@previewjs/plugin-vue3": vue3PluginVersion,
+            "@previewjs/pro": previewjsProVersion,
+            "@previewjs/vfs": vfsVersion,
+          },
+        },
+        null,
+        2
+      ),
+      "utf8"
+    );
+    console.log(`Running pnpm install...`);
+    await fs.promises.unlink(path.join(releaseDirPath, "pnpm-lock.yaml"));
+    await execa("pnpm", ["install", "--lockfile-only"], {
+      cwd: releaseDirPath,
+    });
+    await execa("git", ["add", "."]);
+    if (!(await isGitClean())) {
+      await execa("git", [
+        "commit",
+        "-m",
+        `release: update loader dependencies`,
+      ]);
+      await execa("git", ["push", "origin", "main"]);
+    }
+  }
   const { releaseCli } = await prompt({
     name: "releaseCli",
     type: "confirm",
