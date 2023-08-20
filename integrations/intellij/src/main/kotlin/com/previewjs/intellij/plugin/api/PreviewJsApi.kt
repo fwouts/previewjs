b0ca983d660b1748d6a73669f0a055842836af68
François Wouts <f@zenc.io>
2023-08-20 22:44:35 +1000

    chore: remove unused import

--- integrations/intellij/src/main/kotlin/com/previewjs/intellij/plugin/api/PreviewJsApi.kt
+++ integrations/intellij/src/main/kotlin/com/previewjs/intellij/plugin/api/PreviewJsApi.kt
@@ -6,7 +6,6 @@ import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.Body
-import retrofit2.http.GET
 import retrofit2.http.POST
 import java.util.concurrent.TimeUnit
 
