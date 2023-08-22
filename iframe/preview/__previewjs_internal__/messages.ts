4a01aacf5ddd13b2ef043cf1cb428bc3a81fc3bc
François Wouts <f@zenc.io>
2023-08-22 15:19:42 +1000

    Update messages.ts

--- iframe/preview/__previewjs_internal__/messages.ts
+++ iframe/preview/__previewjs_internal__/messages.ts
@@ -2,7 +2,6 @@ import type { PreviewToAppMessage } from "../../src/messages";
 
 export function sendMessageFromPreview(message: PreviewToAppMessage) {
   (
-    window.__PREVIEWJS_CONTROLLER__.onPreviewMessage ||
-    window.parent.__PREVIEWJS_CONTROLLER__.onPreviewMessage
-  )?.(message);
+    window.__PREVIEWJS_CONTROLLER__ || window.parent.__PREVIEWJS_CONTROLLER__
+  ).onPreviewMessage(message);
 }
