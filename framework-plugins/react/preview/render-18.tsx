0c5bac8f05db45fcb352a5217e2b5cb58e30e27e
François Wouts <f@zenc.io>
2023-08-26 15:24:22 +1000

    refactor(react): simply render implementations

--- framework-plugins/react/preview/render-18.tsx
+++ framework-plugins/react/preview/render-18.tsx
@@ -1,19 +1,11 @@
-import React from "react";
 import type { Root } from "react-dom/client";
 import { createRoot } from "react-dom/client";
 import { rootContainer } from "./root";
 
 let root: Root | null = null;
-export function render(Renderer: React.ComponentType, props: any) {
-  if (!Renderer) {
-    if (root) {
-      root.unmount();
-      root = null;
-    }
-    return;
-  }
+export function render(node: JSX.Element) {
   if (!root) {
     root = createRoot(rootContainer);
   }
-  root.render(<Renderer {...props} />);
+  root.render(node);
 }
