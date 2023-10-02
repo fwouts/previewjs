ee228f4e410e31285cacb972627c1d08cdb457bf
François Wouts <f@zenc.io>
2023-10-02 23:09:25 +1100

    remove-log

--- component-test/src/Foo.tsx
+++ component-test/src/Foo.tsx
@@ -2,10 +2,9 @@ import { isEqual } from "lodash";
 import { Bold } from "./Bold";
 
 export const Foo = () => {
-  console.error("FOO", isEqual(1, 2));
   return (
     <div>
-      Hello from <Bold>test</Bold>!
+      Hello: <Bold>{isEqual(1, 2)}</Bold>!
     </div>
   );
 };
