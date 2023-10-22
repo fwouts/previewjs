9004a68e2b7f67716d8a484c782835be173ce21b
François Wouts <f@zenc.io>
2023-10-23 00:11:40 +1100

    add back

--- core/src/vite/vite-manager.ts
+++ core/src/vite/vite-manager.ts
@@ -248,6 +248,9 @@ export class ViteManager {
     };
     this.options.reader.listeners.add(this.#readerListener);
     try {
+      // PostCSS requires the current directory to change because it relies
+      // on the `import-cwd` package to resolve plugins.
+      process.chdir(this.options.rootDir);
       const configFromProject = await readConfig(
         this.options.rootDir,
         viteLogLevelFromPinoLogger(this.options.logger)
