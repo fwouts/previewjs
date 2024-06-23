db2ff34f413208ff6a6ce958792a1733d9c1b2c0
François Wouts <f@zenc.io>
2024-06-23 11:31:02 +1000

    Better error message

--- chromeless/src/preview.ts
+++ chromeless/src/preview.ts
@@ -108,7 +108,11 @@ export async function startPreview({
       const matchingStory = stories.find((c) => previewableId === c.id);
       if (!matchingComponent && !matchingStory) {
         throw new Error(
-          `Component may be previewable but was not detected by framework plugin: ${previewableId}`
+          `Component may be previewable but was not detected by framework plugin: ${previewableId}.\nDetected components: ${
+            components.map((c) => c.id).join(", ") || "none"
+          }.\nDetected stories: ${
+            stories.map((c) => c.id).join(", ") || "none"
+          }`
         );
       }
       const component = matchingComponent || matchingStory?.associatedComponent;
